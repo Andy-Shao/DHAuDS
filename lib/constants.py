9657ec0260dd8188ea61ef57db34b3278387b534
Andy-Shao <hj13035952619@126.com>
2025-06-04 09:03:17 +0000

    add consists

--- lib/constants.py
+++ lib/constants.py
@@ -1,3 +1,11 @@
 PROJECT_TITLE='NTTA'
 TRAIN_TAG='Train'
 TTA_TAG='TTA'
+
+corruption_dic = {
+    'doing_the_dishes': 'DTD', 
+    'exercise_bike': 'EB',
+    'running_tap': 'RT', 
+    'VocalSound': 'VS',
+    'CochlScene': 'CS'
+}
