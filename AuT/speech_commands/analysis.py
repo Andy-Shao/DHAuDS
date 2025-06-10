851f44aad4f0245ee20844266031987422cf46bb
Andy-Shao <hj13035952619@126.com>
2025-06-10 11:03:33 +0000

    optimization

--- AuT/speech_commands/analysis.py
+++ AuT/speech_commands/analysis.py
@@ -10,12 +10,17 @@ from torch.utils.data import DataLoader
 
 from lib.utils import make_unless_exits, print_argparse, count_ttl_params
 from lib.spdataset import SpeechCommandsV2, VocalSound, BackgroundNoiseDataset, SpeechCommandsV1
-from lib.dataset import RandomChoiceSet
+from lib.dataset import RandomChoiceSet, mlt_store_to, mlt_load_from
 from lib.component import Components, AudioPadding, AmplitudeToDB, MelSpectrogramPadding, FrequenceTokenTransformer
-from lib.component import BackgroundNoiseByFunc
+from lib.component import BackgroundNoiseByFunc, DoNothing
 from lib.acousticDataset import CochlScene
 from AuT.speech_commands.train import build_model
 from AuT.lib.model import FCETransform, AudioClassifier
+
+def clean_cache(cache_path) -> None:
+    import shutil
+    if os.path.exists(cache_path):
+        shutil.rmtree(cache_path)
 
 def multi_train_elect(
         args:argparse.Namespace, softmax:bool, loader:DataLoader,
@@ -114,6 +119,7 @@ if __name__ == '__main__':
     ap.add_argument('--corruption_level', type=float)
     ap.add_argument('--num_workers', type=int, default=16)
     ap.add_argument('--output_path', type=str, default='./result')
+    ap.add_argument('--cache_path', type=str)
     ap.add_argument('--analysis_file', type=str, default='analysis.csv')
     ap.add_argument('--batch_size', type=int, default=64)
     ap.add_argument('--origin_auT_weight', type=str)
@@ -200,13 +206,6 @@ if __name__ == '__main__':
                 data_tf=Components(transforms=[
                     AudioPadding(sample_rate=sample_rate, random_shift=False, max_length=sample_rate),
                     BackgroundNoiseByFunc(noise_level=args.corruption_level, noise_func=noise_source(args, source_type=noise_type), is_random=True),
-                    a_transforms.MelSpectrogram(
-                        sample_rate=sample_rate, n_mels=args.n_mels, n_fft=n_fft, hop_length=hop_length, win_length=win_length,
-                        mel_scale=mel_scale
-                    ), # 80 x 104
-                    AmplitudeToDB(top_db=80., max_out=2.),
-                    MelSpectrogramPadding(target_length=args.target_length),
-                    FrequenceTokenTransformer()
                 ])
             )
         elif args.dataset == 'SpeechCommandsV1':
@@ -215,6 +214,15 @@ if __name__ == '__main__':
                 data_tfs=Components(transforms=[
                     AudioPadding(sample_rate=sample_rate, random_shift=False, max_length=sample_rate),
                     BackgroundNoiseByFunc(noise_level=args.corruption_level, noise_func=noise_source(args, source_type=noise_type), is_random=True),
+                ])
+            )
+        cache_path = os.path.join(args.cache_path, args.dataset, noise_type, str(args.corruption_level))
+        index_file_name = 'metaInfo.csv'
+        mlt_store_to(dataset=corrupted_set, root_path=cache_path, index_file_name=index_file_name, data_tfs=[DoNothing()])
+        corrupted_set = mlt_load_from(
+            root_path=cache_path, index_file_name=index_file_name,
+            data_tfs=[
+                Components(transforms=[
                     a_transforms.MelSpectrogram(
                         sample_rate=sample_rate, n_mels=args.n_mels, n_fft=n_fft, hop_length=hop_length, win_length=win_length,
                         mel_scale=mel_scale
@@ -223,7 +231,8 @@ if __name__ == '__main__':
                     MelSpectrogramPadding(target_length=args.target_length),
                     FrequenceTokenTransformer()
                 ])
-            )
+            ]
+        )
         corrupted_loader = DataLoader(dataset=corrupted_set, batch_size=args.batch_size, shuffle=False, drop_last=False, pin_memory=True, num_workers=args.num_workers)
         accuracy = inference(args=args, auT=auTmodel, auC=clsmodel, data_loader=corrupted_loader)
         print(f'Corrupted testing: accuracy is {accuracy:.4f}%, number of parameters is {param_num}, sample size is {len(corrupted_set)}')
@@ -233,18 +242,20 @@ if __name__ == '__main__':
         auT1, cls1 = build_model(args)
         load_weight(args, mode='adaption1', auT=auT1, auC=cls1)
         auT2, cls2 = build_model(args)
-        load_weight(args, mode='adaption2', auT=auT2, auC=cls2)
-        auT3, cls3 = build_model(args)
-        load_weight(args, mode='adaption3', auT=auT3, auC=cls3)
+        # load_weight(args, mode='adaption2', auT=auT2, auC=cls2)
+        # auT3, cls3 = build_model(args)
+        # load_weight(args, mode='adaption3', auT=auT3, auC=cls3)
         accuracy = inference(args=args, auT=auT1, auC=cls1, data_loader=corrupted_loader)
-        print(f'accuracy is: {accuracy:.4f}%')
+        print(f'accuracy is: {accuracy:.4f}%, number of parameters is: {param_num}, sample size is: {len(corrupted_set)}')
         records.loc[len(records)] = [args.dataset, arch, param_num, noise_type, args.corruption_level, 'CEG', accuracy, 100.-accuracy]
-        accuracy = multi_train_elect(
-            args=args, softmax=args.softmax, loader=corrupted_loader,
-            auT1=auT1, auC1=cls1, auT2=auT2, auC2=cls2,
-            auT3=auT3, auC3=cls3
-        )
-        print(f'Adapted corruption testing: accuracy is {accuracy:.4f}%, number of parameters is {param_num}, sample size is {len(corrupted_set)}')
-        records.loc[len(records)] = [args.dataset, arch, param_num, noise_type, args.corruption_level, 'MLT-TrE', accuracy, 100.-accuracy]
+        # accuracy = multi_train_elect(
+        #     args=args, softmax=args.softmax, loader=corrupted_loader,
+        #     auT1=auT1, auC1=cls1, auT2=auT2, auC2=cls2,
+        #     auT3=auT3, auC3=cls3
+        # )
+        # print(f'Adapted corruption testing: accuracy is {accuracy:.4f}%, number of parameters is {param_num}, sample size is {len(corrupted_set)}')
+        # records.loc[len(records)] = [args.dataset, arch, param_num, noise_type, args.corruption_level, 'MLT-TrE', accuracy, 100.-accuracy]
+
+        clean_cache(cache_path)
 
     records.to_csv(os.path.join(args.output_path, args.analysis_file))
