0df9c75b2b28a06fa6a8baf54af94cc7956ad0fe
Andy-Shao <hj13035952619@126.com>
2025-06-15 14:43:01 +0000

    remove VGGish

--- VGGish/speech_commands/train.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-export BASE_PATH=${BASE_PATH:-'/root'}
-
-python -m VGGish.speech_commands.train --dataset 'SpeechCommandsV1' --dataset_root_path $BASE_PATH'/data/speech_commands_v0.01' \
-     --batch_size 32 --max_epoch 100 --lr '1e-3' --lr_cardinality 50 --lr_gamma 10 --lr_threshold 20 --wandb
