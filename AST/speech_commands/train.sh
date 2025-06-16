3baa52f2b3274b853cd81031d13f9b8f74607357
Andy-Shao <hj13035952619@126.com>
2025-06-16 11:36:56 +0000

    optimization

--- AST/speech_commands/train.sh
+++ AST/speech_commands/train.sh
@@ -2,4 +2,4 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AST.speech_commands.train --dataset 'SpeechCommandsV1'  --dataset_root_path $BASE_PATH'/data/speech_commands_v0.01' \
-    --batch_size 32 --max_epoch 30 --lr '1e-3' --lr_cardinality 50 --lr_gamma 10 --wandb
+    --batch_size 32 --max_epoch 15 --lr '8e-4' --lr_cardinality 50 --lr_gamma 30 --wandb
