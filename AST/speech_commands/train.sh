85e4dfc4150bdc7438c2d771c5cfad3f301cf5e2
Andy-Shao <hj13035952619@126.com>
2025-06-16 08:28:04 +0000

    add AST

---
+++ AST/speech_commands/train.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+python -m AST.speech_commands.train --dataset 'SpeechCommandsV1'  --dataset_root_path $BASE_PATH'/data/speech_commands_v0.01' \
+    --batch_size 32 --max_epoch 30 --lr '1e-3' --lr_cardinality 50 --lr_gamma 10
