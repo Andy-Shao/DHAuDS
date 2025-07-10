aed3b8ce55b8a6138b8a8c3990853ed3e39ac6a6
Andy-Shao <hj13035952619@126.com>
2025-07-10 16:12:52 +0000

    adjust code

--- AST/speech_commands/tta.sh
+++ AST/speech_commands/tta.sh
@@ -1,6 +1,12 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m AST.speech_commands.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+#     --noise_path $BASE_PATH'/data/QUT-NOISE' --cache_path $BASE_PATH'/tmp' \
+#     --max_epoch 200 --lr_cardinality 50 --batch_size 70 --lr '5.5e-5' --num_workers 16 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+#     --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'ENQ' --corruption_level 'L1'
+
 python -m AST.speech_commands.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
     --noise_path $BASE_PATH'/data/QUT-NOISE' --cache_path $BASE_PATH'/tmp' \
     --max_epoch 200 --lr_cardinality 50 --batch_size 70 --lr '5.5e-5' --num_workers 16 \
