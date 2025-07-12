852a0e5d56ffc105070ee7352e7b894185f2d6c7
Andy-Shao <hj13035952619@126.com>
2025-07-12 16:35:20 +0000

    fix bugs

--- AST/speech_commands/ttda.py
+++ AST/speech_commands/ttda.py
@@ -25,7 +25,7 @@ def corrupt_data(args:argparse.Namespace) -> Dataset:
         if args.corruption_level == 'L1':
             snrs = [10, 1, 15]
         elif args.corruption_level == 'L2':
-            snrs = [3, 1, 7]
+            snrs = [5, 1, 10]
         test_set = SpeechCommandsV2(
             root_path=args.dataset_root_path, mode='testing', download=True,
             data_tf=Components(transforms=[
