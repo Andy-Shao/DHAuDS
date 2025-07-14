352a2be6b177a4d39c6ddec60c1f569ace866dc6
Andy-Shao <hj13035952619@126.com>
2025-07-14 16:01:04 +0000

    finish the analysis

--- AST/speech_commands/analysis.sh
+++ AST/speech_commands/analysis.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
-# python -m AST.speech_commands.analysis --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-#     --noise_path $BASE_PATH'/data/QUT-NOISE' --cache_path $BASE_PATH'/tmp' \
-#     --corruption_type 'ENQP' --corruption_level 'L2' --num_workers 16 --batch_size 70 \
-#     --adpt_wght_pth './result/SpeechCommandsV2/AST/TTDA/AST-SC2-ENQ-L2.pt'
-
 python -m AST.speech_commands.analysis --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-    --cache_path $BASE_PATH'/tmp' \
-    --corruption_type 'WHNP' --corruption_level 'L2' --num_workers 16 --batch_size 70 \
-    --adpt_wght_pth './result/SpeechCommandsV2/AST/TTDA/AST-SC2-WHN-L2.pt'
+    --noise_path $BASE_PATH'/data/QUT-NOISE' --cache_path $BASE_PATH'/tmp' \
+    --corruption_type 'ENQP' --corruption_level 'L2' --num_workers 16 --batch_size 70 \
+    --adpt_wght_pth './result/SpeechCommandsV2/AST/TTDA/AST-SC2-ENQP-L2.pt'
+
+# python -m AST.speech_commands.analysis --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+#     --cache_path $BASE_PATH'/tmp' \
+#     --corruption_type 'WHNP' --corruption_level 'L2' --num_workers 16 --batch_size 70 \
+#     --adpt_wght_pth './result/SpeechCommandsV2/AST/TTDA/AST-SC2-WHNP-L2.pt'
