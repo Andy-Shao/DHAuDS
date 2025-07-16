782b18abc3953a2c1c431a099578cb29403068de
Andy-Shao <hj13035952619@126.com>
2025-07-16 17:05:23 +0000

    optimization

--- AST/CochlScene/train.sh
+++ AST/CochlScene/train.sh
@@ -2,4 +2,4 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AST.CochlScene.train --dataset 'CochlScene' --dataset_root_path $BASE_PATH'/data/CochlScene' \
-    --batch_size 30 --max_epoch 30 --lr '1e-3' --lr_gamma 30 --wandb
+    --batch_size 30 --max_epoch 10 --lr '8e-4' --lr_gamma 30 --wandb
