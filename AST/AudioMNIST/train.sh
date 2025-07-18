6e02cdc2b62bebb843f97eadb95bd052c310613e
Andy-Shao <hj13035952619@126.com>
2025-07-18 13:07:34 +0000

    fix bugs

--- AST/AudioMNIST/train.sh
+++ AST/AudioMNIST/train.sh
@@ -2,4 +2,4 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AST.AudioMNIST.train --dataset 'AudioMINST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-    --batch_size 32 --max_epoch 10 --lr '1e-4' --lr_gamma 30
+    --batch_size 32 --max_epoch 10 --lr '1e-4' --lr_gamma 30 --wandb
