90f83e5ee64b4039ce4e0fe362693b51f8686837
Andy-Shao <hj13035952619@126.com>
2025-07-21 07:26:14 +0000

    clean code

--- HuBERT/speech_commands/train.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-export BASE_PATH=${BASE_PATH:-'/root'}
-
-python -m HuBERT.speech_commands.train --dataset 'SpeechCommandsV1'  --dataset_root_path $BASE_PATH'/data/speech_commands_v0.01' \
-    --batch_size 32 --max_epoch 30 --lr '8e-4' --model_level 'base' --lr_cardinality 50 --lr_gamma 30 \
-    --use_pre_trained_weigth --wandb
