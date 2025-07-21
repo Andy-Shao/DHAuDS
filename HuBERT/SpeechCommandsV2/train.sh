2b56926952b347c215f75d5dd401d4d0fd9e96c6
Andy-Shao <hj13035952619@126.com>
2025-07-21 14:50:23 +0000

    add SpeechCommands V2

--- HuBERT/SpeechCommandsV2/train.sh
+++ HuBERT/SpeechCommandsV2/train.sh
@@ -2,5 +2,5 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m HuBERT.SpeechCommandsV2.train --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-    --batch_size 32 --lr '8e-4' --max_epoch 14 --model_level 'base' --use_pre_trained_weigth \
+    --batch_size 32 --lr '8e-4' --max_epoch 30 --model_level 'base' --use_pre_trained_weigth \
     --lr_cardinality 50 --lr_gamma 30 --wandb
