fd72dda055a0e39465be3b4ddc7ba25c3d3677b8
Andy-Shao <hj13035952619@126.com>
2025-07-25 08:48:59 +0000

    optimization

--- HuBERT/VocalSound/analysis.sh
+++ HuBERT/VocalSound/analysis.sh
@@ -2,7 +2,7 @@
 export BASE_PATH='/root'
 
 python -m HuBERT.VocalSound.analysis --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
-    --cache_path $BASE_PATH'/tmp' \
+    --cache_path $BASE_PATH'/tmp' --repeat_time 3 \
     --corruption_type 'WHN' --corruption_level 'L2' --batch_size 32 \
     --hub_wght_pth './result/VocalSound/HuBERT/train/hubert-base-VS.pt' \
     --clsf_wght_pth './result/VocalSound/HuBERT/train/clsModel-base-VS.pt' \
