6d353c68def8d5cdae73690f4f58852e35452b03
Andy-Shao <hj13035952619@126.com>
2025-07-31 16:57:36 +0000

    finished the adjust

--- lib/acousticDataset.py
+++ lib/acousticDataset.py
@@ -11,7 +11,7 @@ class ReefSet(Dataset):
     def __init__(
             self, root_path:str, mode:str, include_rate:bool=False, data_tf:nn.Module=None, label_tf:nn.Module=None,
             train_annotations_file:str='./data/ReefSet/train_annotations.csv', 
-            test_annotations_file:str='./data/ReefSet/test_annotations.csv'
+            test_annotations_file:str='./data/ReefSet/test_annotations.csv', label_mode:str='single'
         ):
         super().__init__()
         self.root_path = root_path
@@ -21,7 +21,7 @@ class ReefSet(Dataset):
         self.data_tf = data_tf
         self.label_tf = label_tf
         self.meta_info = pd.read_csv(train_annotations_file) if mode == 'train' else pd.read_csv(test_annotations_file)
-        self.label_dic = self.__label_dic__()
+        self.label_dic = self.__label_dic__(label_mode)
 
     def __len__(self):
         return len(self.meta_info)
@@ -42,8 +42,9 @@ class ReefSet(Dataset):
         else:
             return wavform, label
     
-    def __label_dic__(self) -> dict:
-        return {
+    def __label_dic__(self, label_mode) -> dict:
+        if label_mode == 'multiple':
+            return {
             'ambient': [0], 
 
             'anthrop_boat_engine': [1],
@@ -84,7 +85,50 @@ class ReefSet(Dataset):
             'bioph_whup': [4, 35], 
 
             'geoph_waves': [36]
-        }
+            }
+        else:
+            return {
+            'ambient': [0], 
+
+            'anthrop_boat_engine': [1],
+            'anthrop_bomb': [2],
+            'anthrop_mechanical': [3],
+
+            'bioph': [4],
+            'bioph_cascading_saw': [5],
+            'bioph_chatter': [6],
+            'bioph_chorus': [7],
+            'bioph_crackle': [8],
+            'bioph_croak': [9],
+            'bioph_damselfish': [10],
+            'bioph_dolphin': [11],
+            'bioph_double_pulse': [12],
+            'bioph_echinidae': [13],
+            'bioph_epigut': [14],
+            'bioph_grazing': [15],
+            'bioph_grouper_a': [16],
+            'bioph_grouper_groan': [17],
+            'bioph_growl': [18],
+            'bioph_holocentrus': [19],
+            'bioph_knock': [20],
+            'bioph_knock_croak_a': [21],
+            'bioph_knock_croak_b': [22],
+            'bioph_knock_croak_c': [23],
+            'bioph_low_growl': [24],
+            'bioph_megnov': [25],
+            'bioph_midshipman': [26],
+            'bioph_mycbon': [27],
+            'bioph_pomamb': [28],
+            'bioph_pulse': [29],
+            'bioph_rattle': [30],
+            'bioph_rattle_response': [31],
+            'bioph_series_a': [32],
+            'bioph_series_b': [33],
+            'bioph_stridulation': [34],
+            'bioph_whup': [35], 
+
+            'geoph_waves': [36]
+            }
         
 
 class DEMAND(Dataset):
