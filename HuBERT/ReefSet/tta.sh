b85f47c42a5fa73e9405903231b2c8c02896385f
Andy-Shao <hj13035952619@126.com>
2025-08-03 10:05:05 +0000

    optimization

--- HuBERT/ReefSet/tta.sh
+++ HuBERT/ReefSet/tta.sh
@@ -3,7 +3,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 
 # python -m HuBERT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
 #     --cache_path $BASE_PATH'/tmp' \
-#     --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
+#     --max_epoch 6 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
 #     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
 #     --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --corruption_type 'WHN' --corruption_level 'L2' \
 #     --hub_wght_pth './result/ReefSet/HuBERT/train/hubert-base-RS.pt' \
@@ -25,10 +25,18 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --hub_wght_pth './result/ReefSet/HuBERT/train/hubert-base-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/HuBERT/train/clsModel-base-RS.pt' --wandb
 
+# python -m HuBERT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+#     --cache_path $BASE_PATH'/tmp' \
+#     --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.35 --num_workers 16 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+#     --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --corruption_type 'TST' --corruption_level 'L2' \
+#     --hub_wght_pth './result/ReefSet/HuBERT/train/hubert-base-RS.pt' \
+#     --clsf_wght_pth './result/ReefSet/HuBERT/train/clsModel-base-RS.pt' --wandb
+
 python -m HuBERT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
-    --cache_path $BASE_PATH'/tmp' \
+    --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/QUT-NOISE' \
     --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.35 --num_workers 16 \
     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
-    --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --corruption_type 'TST' --corruption_level 'L2' \
+    --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --corruption_type 'ENQ' --corruption_level 'L2' \
     --hub_wght_pth './result/ReefSet/HuBERT/train/hubert-base-RS.pt' \
     --clsf_wght_pth './result/ReefSet/HuBERT/train/clsModel-base-RS.pt' --wandb
