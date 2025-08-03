23d8e1d2eb981a0c1d29c1a18917ae570c35520f
Andy-Shao <hj13035952619@126.com>
2025-08-03 12:38:49 +0000

    add TTA

--- HuBERT/ReefSet/tta.sh
+++ HuBERT/ReefSet/tta.sh
@@ -33,10 +33,18 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --hub_wght_pth './result/ReefSet/HuBERT/train/hubert-base-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/HuBERT/train/clsModel-base-RS.pt' --wandb
 
+# python -m HuBERT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+#     --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/QUT-NOISE' \
+#     --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.35 --num_workers 16 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+#     --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --corruption_type 'ENQ' --corruption_level 'L2' \
+#     --hub_wght_pth './result/ReefSet/HuBERT/train/hubert-base-RS.pt' \
+#     --clsf_wght_pth './result/ReefSet/HuBERT/train/clsModel-base-RS.pt' --wandb
+
 python -m HuBERT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
-    --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/QUT-NOISE' \
-    --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.35 --num_workers 16 \
+    --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/DEMAND_16k' \
+    --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '5e-5' --hub_lr_decay 0.35 --num_workers 16 \
     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
-    --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --corruption_type 'ENQ' --corruption_level 'L2' \
+    --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --corruption_type 'END1' --corruption_level 'L2' \
     --hub_wght_pth './result/ReefSet/HuBERT/train/hubert-base-RS.pt' \
     --clsf_wght_pth './result/ReefSet/HuBERT/train/clsModel-base-RS.pt' --wandb
