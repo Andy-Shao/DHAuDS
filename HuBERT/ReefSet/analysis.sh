fd00004235f9be3563303625ba9afc2b0c3dc8d5
Andy-Shao <hj13035952619@126.com>
2025-08-06 14:18:57 +0000

    clean code

--- HuBERT/ReefSet/analysis.sh
+++ HuBERT/ReefSet/analysis.sh
@@ -2,11 +2,11 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 ## Generate Corruption sets
-# python -m HuBERT.ReefSet.corrupt_set --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
-#     --batch_size 33 --output_path $BASE_PATH'/data/ReefSet-C' --seed 2025 --num_workers 8 \
-#     --enq_path $BASE_PATH'/data/QUT-NOISE' \
-#     --end_path $BASE_PATH'/data/DEMAND_16k' \
-#     --ensc_path $BASE_PATH'/data'
+python -m HuBERT.ReefSet.corrupt_set --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+    --batch_size 33 --output_path $BASE_PATH'/data/ReefSet-C' --seed 123456 --num_workers 8 \
+    --enq_path $BASE_PATH'/data/QUT-NOISE' \
+    --end_path $BASE_PATH'/data/DEMAND_16k' \
+    --ensc_path $BASE_PATH'/data'
 
 ## Analysis
 python -m HuBERT.ReefSet.analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet-C' \
