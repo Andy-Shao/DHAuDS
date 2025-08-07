cfff23ab2650c536bce9b813053d94c8f66482cf
Andy-Shao <hj13035952619@126.com>
2025-08-07 12:02:57 +0000

    add analysis

--- HuBERT/UrbanSound8K/analysis.py
+++ HuBERT/UrbanSound8K/analysis.py
@@ -1,10 +1,49 @@
 import argparse
 import os
+import pandas as pd
 
 import torch 
+from torch.utils.data import DataLoader
+from torchaudio.transforms import Resample
 
-from lib.utils import make_unless_exits, print_argparse
+from lib.utils import make_unless_exits, print_argparse, count_ttl_params
+from lib.corruption import corruption_meta, UrbanSound8KC
+from lib.component import Components, AudioClip, ReduceChannel
+from HuBERT.UrbanSound8K.train import inference, build_model
+from HuBERT.ReefSet.analysis import load_weight
 
+def analyzing(args:argparse.Namespace, corruption_types:list[str], corruption_levels:list[str]) -> None:
+    records = pd.DataFrame(columns=['Dataset',  'Algorithm', 'Param No.', 'Corruption', 'Non-adapted', 'Adapted'])
+    corruption_metas = corruption_meta(corrupytion_types=corruption_types, corruption_levels=corruption_levels)
+    hubert, clsf = build_model(args=args, pre_weight=args.use_pre_trained_weigth)
+    load_weight(args=args, hubert=hubert, clsf=clsf, mode='origin')
+    param_no = count_ttl_params(hubert) + count_ttl_params(clsf)
+
+    for idx, cmeta in enumerate(corruption_metas):
+        print(f'{idx+1}/{len(corruption_metas)}: {args.dataset} {cmeta.type}-{cmeta.level} analyzing...')
+        adpt_hubert, adpt_clsf = build_model(args=args, pre_weight=args.use_pre_trained_weigth)
+        load_weight(args=args, hubert=adpt_hubert, clsf=adpt_clsf, mode='adaptation', metaInfo=cmeta)
+
+        adpt_set = UrbanSound8KC(
+            root_path=args.dataset_root_path, corruption_type=cmeta.type, corruption_level=cmeta.level,
+            data_tf=Components(transforms=[
+                Resample(orig_freq=args.orig_sample_rate, new_freq=args.sample_rate),
+                AudioClip(max_length=args.audio_length, mode='head', is_random=False),
+                ReduceChannel()
+            ])
+        )
+        adpt_loader = DataLoader(
+            dataset=adpt_set, batch_size=args.batch_size, shuffle=False, drop_last=False, pin_memory=True,
+            num_workers=args.num_workers
+        )
+
+        print('Non-adaptation analyzing...')
+        orig_f1 = inference(args=args, hubert=hubert, clsModel=clsf, data_loader=adpt_loader)
+        print('Adaptation analyzing...')
+        adpt_f1 = inference(args=args, hubert=adpt_hubert, clsModel=adpt_clsf, data_loader=adpt_loader)
+        print(f'{args.dataset} {cmeta.type}-{cmeta.level} non-adapted f1: {orig_f1:.4f}, adapted f1: {adpt_f1:.4f}')
+        records.loc[len(records)] = [args.dataset, args.arch, param_no, f'{cmeta.type}-{cmeta.level}', orig_f1, adpt_f1]
+    records.to_csv(os.path.join(args.output_path, args.output_file_name))
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
@@ -24,6 +63,7 @@ if __name__ == '__main__':
     if args.dataset == 'UrbanSound8K':
         args.class_num = 10
         args.sample_rate = 16000
+        args.orig_sample_rate = 44100
         args.audio_length = int(4 * 16000)
     else:
         raise Exception('No support!')
@@ -35,3 +75,4 @@ if __name__ == '__main__':
 
     print_argparse(args)
     ##########################################
+    analyzing(args=args, corruption_types=['WHN'], corruption_levels=['L2']) 
--- HuBERT/UrbanSound8K/analysis.sh
+++ HuBERT/UrbanSound8K/analysis.sh
@@ -2,5 +2,11 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 # Generate data
-python -m HuBERT.UrbanSound8K.corrupt_set --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
-    --output_path $BASE_PATH'/data/UrbanSound8K-C' --batch_size 33 --seed 123456 --ensc_path $BASE_PATH'/data'
+# python -m HuBERT.UrbanSound8K.corrupt_set --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
+#     --output_path $BASE_PATH'/data/UrbanSound8K-C' --batch_size 33 --seed 123456 --ensc_path $BASE_PATH'/data'
+
+# Analysis data
+python -m HuBERT.UrbanSound8K.analysis --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K-C' \
+    --batch_size 33 --output_file_name 'UrbanSound8K_analysis.csv' --use_pre_trained_weigth --model_level 'base' \
+    --orig_wght_pth './result/UrbanSound8K/HuBERT/train' \
+    --adpt_wght_path './result/UrbanSound8K/HuBERT/TTDA'
