ece5713844bde91c95231d8d543fc7dca503f3c3
Andy-Shao <hj13035952619@126.com>
2025-08-16 08:03:12 +0000

    TST L1 for US8

--- HuBERT/UrbanSound8K/tta.sh
+++ HuBERT/UrbanSound8K/tta.sh
@@ -41,13 +41,13 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --hub_wght_pth './result/UrbanSound8K/HuBERT/train/hubert-base-US8.pt' \
 #     --clsf_wght_pth './result/UrbanSound8K/HuBERT/train/clsModel-base-US8.pt' --wandb
 
-python -m HuBERT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
-    --cache_path $BASE_PATH'/tmp' \
-    --max_epoch 10 --lr_cardinality 50 --batch_size 33 --lr '5e-5' --hub_lr_decay 0.35 --num_workers 16 \
-    --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --lr_momentum 0.75 \
-    --ent_rate 0.01 --gent_rate 0.01 --gent_q 1.1 --corruption_type 'PSH' --corruption_level 'L1' \
-    --hub_wght_pth './result/UrbanSound8K/HuBERT/train/hubert-base-US8.pt' \
-    --clsf_wght_pth './result/UrbanSound8K/HuBERT/train/clsModel-base-US8.pt' --wandb
+# python -m HuBERT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
+#     --cache_path $BASE_PATH'/tmp' \
+#     --max_epoch 10 --lr_cardinality 50 --batch_size 33 --lr '5e-5' --hub_lr_decay 0.35 --num_workers 16 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --lr_momentum 0.75 \
+#     --ent_rate 0.01 --gent_rate 0.01 --gent_q 1.1 --corruption_type 'PSH' --corruption_level 'L1' \
+#     --hub_wght_pth './result/UrbanSound8K/HuBERT/train/hubert-base-US8.pt' \
+#     --clsf_wght_pth './result/UrbanSound8K/HuBERT/train/clsModel-base-US8.pt' --wandb
 
 # python -m HuBERT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
 #     --cache_path $BASE_PATH'/tmp' \
@@ -56,3 +56,11 @@ python -m HuBERT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path 
 #     --ent_rate 0.01 --gent_rate 0.01 --gent_q 1.1 --corruption_type 'TST' --corruption_level 'L2' \
 #     --hub_wght_pth './result/UrbanSound8K/HuBERT/train/hubert-base-US8.pt' \
 #     --clsf_wght_pth './result/UrbanSound8K/HuBERT/train/clsModel-base-US8.pt' --wandb
+
+python -m HuBERT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
+    --cache_path $BASE_PATH'/tmp' \
+    --max_epoch 10 --lr_cardinality 50 --batch_size 33 --lr '1e-4' --hub_lr_decay 0.35 --num_workers 16 \
+    --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+    --ent_rate 0.01 --gent_rate 0.01 --gent_q 1.1 --corruption_type 'TST' --corruption_level 'L1' \
+    --hub_wght_pth './result/UrbanSound8K/HuBERT/train/hubert-base-US8.pt' \
+    --clsf_wght_pth './result/UrbanSound8K/HuBERT/train/clsModel-base-US8.pt' --wandb
