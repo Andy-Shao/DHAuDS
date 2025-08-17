86c62bd79c5028d6ec6e83bae6aa4e00a9166ed3
Andy-Shao <hj13035952619@126.com>
2025-08-17 12:08:03 +0000

    add END2 L1 for VS

--- HuBERT/VocalSound/tta.sh
+++ HuBERT/VocalSound/tta.sh
@@ -41,13 +41,13 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --hub_wght_pth './result/VocalSound/HuBERT/train/hubert-base-VS.pt' \
 #     --clsf_wght_pth './result/VocalSound/HuBERT/train/clsModel-base-VS.pt' --wandb
 
-python -m HuBERT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
-    --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/DEMAND_16k' \
-    --max_epoch 15 --lr_cardinality 50 --batch_size 32 --lr '1e-4' --hub_lr_decay 0.55 --num_workers 16 \
-    --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --lr_momentum 0.75 \
-    --ent_rate 0.05 --gent_rate 0.05 --gent_q 1.1 --corruption_type 'END1' --corruption_level 'L1' \
-    --hub_wght_pth './result/VocalSound/HuBERT/train/hubert-base-VS.pt' \
-    --clsf_wght_pth './result/VocalSound/HuBERT/train/clsModel-base-VS.pt' --wandb
+# python -m HuBERT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+#     --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/DEMAND_16k' \
+#     --max_epoch 15 --lr_cardinality 50 --batch_size 32 --lr '1e-4' --hub_lr_decay 0.55 --num_workers 16 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --lr_momentum 0.75 \
+#     --ent_rate 0.05 --gent_rate 0.05 --gent_q 1.1 --corruption_type 'END1' --corruption_level 'L1' \
+#     --hub_wght_pth './result/VocalSound/HuBERT/train/hubert-base-VS.pt' \
+#     --clsf_wght_pth './result/VocalSound/HuBERT/train/clsModel-base-VS.pt' --wandb
 
 # python -m HuBERT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/DEMAND_16k' \
@@ -56,6 +56,14 @@ python -m HuBERT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BAS
 #     --ent_rate 0.05 --gent_rate 0.05 --gent_q 1.1 --corruption_type 'END2' --corruption_level 'L2' \
 #     --hub_wght_pth './result/VocalSound/HuBERT/train/hubert-base-VS.pt' \
 #     --clsf_wght_pth './result/VocalSound/HuBERT/train/clsModel-base-VS.pt' --wandb
+
+python -m HuBERT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+    --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/DEMAND_16k' \
+    --max_epoch 15 --lr_cardinality 50 --batch_size 32 --lr '1e-4' --hub_lr_decay 0.55 --num_workers 16 \
+    --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+    --ent_rate 0.05 --gent_rate 0.05 --gent_q 1.1 --corruption_type 'END2' --corruption_level 'L1' \
+    --hub_wght_pth './result/VocalSound/HuBERT/train/hubert-base-VS.pt' \
+    --clsf_wght_pth './result/VocalSound/HuBERT/train/clsModel-base-VS.pt' --wandb
 
 # python -m HuBERT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data' \
