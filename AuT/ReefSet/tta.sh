4bc70995b972c7bff47ba14997bd39007fee23fd
Andy-Shao <hj13035952619@126.com>
2025-08-21 14:24:44 +0000

    add END2 L2 for RS on AuT

--- AuT/ReefSet/tta.sh
+++ AuT/ReefSet/tta.sh
@@ -9,10 +9,18 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
+# python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+#     --noise_path $BASE_PATH'/data/DEMAND_16k' \
+#     --corruption_type 'END1' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 15 --lr '5e-5' --aut_lr_decay 0.55 --lr_momentum 0.70 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
+#     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
+#     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
+
 python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
     --noise_path $BASE_PATH'/data/DEMAND_16k' \
-    --corruption_type 'END1' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 15 --lr '5e-5' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-    --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
+    --corruption_type 'END2' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
+    --nucnm_rate 1.0 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 \
     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
