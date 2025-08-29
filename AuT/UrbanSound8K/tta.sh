b390afff5a7bd8db35ac8d99bc09fb5d0f32b0d7
Andy-Shao <hj13035952619@126.com>
2025-08-29 09:10:48 +0000

    add ENSC L2 for US8 on AuT

--- AuT/UrbanSound8K/tta.sh
+++ AuT/UrbanSound8K/tta.sh
@@ -1,9 +1,17 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
+#     --noise_path $BASE_PATH'/data' \
+#     --corruption_type 'ENSC' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 30 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
+#     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
+#     --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
+
 python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
     --noise_path $BASE_PATH'/data' \
-    --corruption_type 'ENSC' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
+    --corruption_type 'ENSC' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
     --max_epoch 30 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
