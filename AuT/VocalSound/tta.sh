cd17a0e03aa2ed2aa9fbfa5bea641cf1f62fb38e
Andy-Shao <hj13035952619@126.com>
2025-09-06 13:16:23 +0000

    optimization

--- AuT/VocalSound/tta.sh
+++ AuT/VocalSound/tta.sh
@@ -3,8 +3,8 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
     --cache_path $BASE_PATH'/tmp' \
-    --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+    --max_epoch 30 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
     --lr_gamma 30 --lr_threshold 35 --corruption_type 'WHN' --corruption_level 'L1' \
-    --nucnm_rate 1.0 --ent_rate '1e-2' --gent_rate 0.5 --gent_q 1.1 \
+    --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.5 --gent_q 1.1 \
     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
     --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
