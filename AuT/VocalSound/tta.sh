14cee5c3e62ad76e4b782feda3775250ae2f0d48
Andy-Shao <hj13035952619@126.com>
2025-09-08 15:31:50 +0000

    WHN L2 for VS on AuT

--- AuT/VocalSound/tta.sh
+++ AuT/VocalSound/tta.sh
@@ -1,25 +1,25 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+#     --cache_path $BASE_PATH'/tmp' \
+#     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+#     --lr_gamma 30 --lr_threshold 35 --corruption_type 'WHN' --corruption_level 'L1' \
+#     --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.25 --gent_q 1.1 \
+#     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
+#     --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
+
 python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
     --cache_path $BASE_PATH'/tmp' \
     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
-    --lr_gamma 30 --lr_threshold 35 --corruption_type 'WHN' --corruption_level 'L1' \
-    --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.25 --gent_q 1.1 \
+    --lr_gamma 30 --lr_threshold 35 --corruption_type 'WHN' --corruption_level 'L2' \
+    --nucnm_rate 1.0 --ent_rate 0.25 --gent_rate 0.0 --gent_q 0.9 \
     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
     --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
 
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
-#     --cache_path $BASE_PATH'/tmp' \
-#     --max_epoch 30 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
-#     --lr_gamma 30 --lr_threshold 35 --corruption_type 'WHN' --corruption_level 'L2' \
-#     --nucnm_rate 1.0 --ent_rate 0.25 --gent_rate 0.25 --gent_q 0.9 \
-#     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
-#     --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
-
-# python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data' \
-#     --max_epoch 30 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+#     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
 #     --lr_gamma 30 --lr_threshold 35 --corruption_type 'ENSC' --corruption_level 'L1' \
 #     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
 #     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
@@ -27,7 +27,7 @@ python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_P
 
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data' \
-#     --max_epoch 30 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+#     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
 #     --lr_gamma 30 --lr_threshold 35 --corruption_type 'ENSC' --corruption_level 'L2' \
 #     --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 \
 #     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
@@ -35,7 +35,7 @@ python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_P
 
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp'\
-#     --max_epoch 30 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+#     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
 #     --lr_gamma 30 --lr_threshold 35 --corruption_type 'PSH' --corruption_level 'L1' \
 #     --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --gent_q 1.1 \
 #     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
@@ -43,7 +43,7 @@ python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_P
 
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' \
-#     --max_epoch 30 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+#     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
 #     --lr_gamma 30 --lr_threshold 35 --corruption_type 'PSH' --corruption_level 'L2' \
 #     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 0.0 --gent_q 1.1 \
 #     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
@@ -51,7 +51,7 @@ python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_P
 
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' \
-#     --max_epoch 60 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+#     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
 #     --lr_gamma 30 --lr_threshold 35 --corruption_type 'TST' --corruption_level 'L1' \
 #     --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --gent_q 1.1 \
 #     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
