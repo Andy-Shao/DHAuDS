0eb97209e33f33d62bf1088061daddac5f3ef63e
Andy-Shao <hj1303595219@126.com>
2025-09-09 11:00:24 +0000

    TST L1 for VS on AuT

--- AuT/VocalSound/tta.sh
+++ AuT/VocalSound/tta.sh
@@ -41,21 +41,21 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
 #     --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
 
-python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
-    --cache_path $BASE_PATH'/tmp' \
-    --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
-    --lr_gamma 30 --lr_threshold 35 --corruption_type 'PSH' --corruption_level 'L2' \
-    --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
-    --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
-    --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
-
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' \
 #     --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
-#     --lr_gamma 30 --lr_threshold 35 --corruption_type 'TST' --corruption_level 'L1' \
-#     --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --gent_q 1.1 \
+#     --lr_gamma 30 --lr_threshold 35 --corruption_type 'PSH' --corruption_level 'L2' \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
 #     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
 #     --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
+
+python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+    --cache_path $BASE_PATH'/tmp' \
+    --max_epoch 45 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+    --lr_gamma 30 --lr_threshold 35 --corruption_type 'TST' --corruption_level 'L1' \
+    --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 1.0 --gent_q 1.1 \
+    --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
+    --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
 
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' \
