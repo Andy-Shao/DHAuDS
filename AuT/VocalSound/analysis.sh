bafb93dda5675c56603437df83ce0c4342ee79c4
Andy-Shao <hj13035952619@126.com>
2025-09-10 11:47:11 +0000

    add analysis for VS on AuT

--- AuT/VocalSound/analysis.sh
+++ AuT/VocalSound/analysis.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
-python -m AuT.VocalSound.analysis --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+python -m AuT.VocalSound.analysis --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/VocalSound-C' \
     --batch_size 32 --output_file_name 'VocalSound_analysis.csv' \
-    --orig_wght_pth './result/VocalSound/AMAuT/train'
+    --orig_wght_pth './result/VocalSound/AMAuT/train' \
+    --adpt_wght_path './result/VocalSound/AMAuT/TTDA'
