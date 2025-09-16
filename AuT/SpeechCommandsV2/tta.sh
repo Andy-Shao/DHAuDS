ae5bcd87337e7faf9ee0daa4a80bfd871cc2c1d0
Andy-Shao <hj13035952619@126.com>
2025-09-16 10:11:59 +0000

    optimization

--- AuT/SpeechCommandsV2/tta.sh
+++ AuT/SpeechCommandsV2/tta.sh
@@ -41,18 +41,18 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #    --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
 #    --clsf_wght_pth './result/SpeechCommandsV2/AMAuT/train/clsf-SC2.pt' --wandb
 
-# python -m AuT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-#     --cache_path $BASE_PATH'/tmp' \
-#     --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
-#     --lr_gamma 30 --lr_threshold 35 --corruption_type 'PSH' --corruption_level 'L2' \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 0.1 --gent_q 1.1 \
-#     --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
-#     --clsf_wght_pth './result/SpeechCommandsV2/AMAuT/train/clsf-SC2.pt' --wandb
-
 python -m AuT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-   --cache_path $BASE_PATH'/tmp' \
-   --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
-   --lr_gamma 30 --lr_threshold 35 --corruption_type 'TST' --corruption_level 'L1' \
-   --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 0.0 --gent_q 1.1 \
-   --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
-   --clsf_wght_pth './result/SpeechCommandsV2/AMAuT/train/clsf-SC2.pt' --wandb
+    --cache_path $BASE_PATH'/tmp' \
+    --max_epoch 60 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+    --lr_gamma 30 --lr_threshold 35 --corruption_type 'PSH' --corruption_level 'L2' \
+    --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 0.1 --gent_q 1.1 \
+    --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
+    --clsf_wght_pth './result/SpeechCommandsV2/AMAuT/train/clsf-SC2.pt' --wandb
+
+# python -m AuT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+#    --cache_path $BASE_PATH'/tmp' \
+#    --max_epoch 15 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+#    --lr_gamma 30 --lr_threshold 35 --corruption_type 'TST' --corruption_level 'L1' \
+#    --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 0.0 --gent_q 1.1 \
+#    --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
+#    --clsf_wght_pth './result/SpeechCommandsV2/AMAuT/train/clsf-SC2.pt' --wandb
