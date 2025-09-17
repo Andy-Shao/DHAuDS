10cccf586b5b7bd11c38a6788fa954da98c0231d
Andy-Shao <hj13035952619@126.com>
2025-09-17 14:10:17 +0000

    optimization

--- AuT/SpeechCommandsV2/tta.sh
+++ AuT/SpeechCommandsV2/tta.sh
@@ -67,7 +67,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AuT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
    --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/QUT-NOISE' \
-   --max_epoch 20 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+   --max_epoch 60 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
    --lr_gamma 30 --lr_threshold 35 --corruption_type 'ENQ' --corruption_level 'L1' \
    --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 2.0 --lr_momentum 0.75 \
    --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
