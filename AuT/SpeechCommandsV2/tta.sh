beb605ca21a4440a90340c707fc48a3544ac5808
Andy-Shao <hj13035952619@126.com>
2025-09-17 11:44:58 +0000

    add ENQ L1 for SC2 on AuT

--- AuT/SpeechCommandsV2/tta.sh
+++ AuT/SpeechCommandsV2/tta.sh
@@ -64,3 +64,11 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #   --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.6 \
 #   --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
 #   --clsf_wght_pth './result/SpeechCommandsV2/AMAuT/train/clsf-SC2.pt' --wandb
+
+python -m AuT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+   --cache_path $BASE_PATH'/tmp' --noise_path $BASE_PATH'/data/QUT-NOISE' \
+   --max_epoch 20 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --aut_lr_decay 0.55 --num_workers 16 \
+   --lr_gamma 30 --lr_threshold 35 --corruption_type 'ENQ' --corruption_level 'L1' \
+   --nucnm_rate 0.0 --ent_rate 1.0 --gent_rate 0.0 --gent_q 1.1 \
+   --aut_wght_pth './result/SpeechCommandsV2/AMAuT/train/aut-SC2.pt' \
+   --clsf_wght_pth './result/SpeechCommandsV2/AMAuT/train/clsf-SC2.pt' 
