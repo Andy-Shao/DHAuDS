33bab72ebbf82c5746b944e7269432e96d68d953
Andy-Shao <hj13035952619@126.com>
2025-09-27 11:25:03 +0000

    use mse for US8 on AuT

--- AuT/UrbanSound8K/tta.sh
+++ AuT/UrbanSound8K/tta.sh
@@ -5,7 +5,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --noise_path $BASE_PATH'/data' \
 #     --corruption_type 'ENSC' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 30 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --js_rate 0.1 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.1 \
 #     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
 #     --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
 
@@ -13,16 +13,16 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --noise_path $BASE_PATH'/data' \
 #     --corruption_type 'ENSC' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 30 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --js_rate 0.1 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.1 \
 #     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
 #     --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
 
-# python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
-#     --corruption_type 'WHN' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
-#     --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --js_rate 0.1 \
-#     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
-#     --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
+python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
+    --corruption_type 'WHN' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
+    --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.1 \
+    --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
+    --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
 
 # python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
 #     --corruption_type 'WHN' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
@@ -52,9 +52,9 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
 #     --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
 
-python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
-    --corruption_type 'PSH' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-    --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --js_rate 0.1 \
-    --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
-    --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
+# python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/UrbanSound8K' \
+#     --corruption_type 'PSH' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --js_rate 0.1 \
+#     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
+#     --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
