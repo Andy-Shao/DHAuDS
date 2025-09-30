48a5545fdaacda90d658e0e7319b16d5cf5b9871
Andy-Shao <hj13035952619@126.com>
2025-09-30 09:15:13 +0000

    optimization

--- AuT/ReefSet/tta.sh
+++ AuT/ReefSet/tta.sh
@@ -1,19 +1,19 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
-python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
-    --noise_path $BASE_PATH'/data/DEMAND_16k' \
-    --corruption_type 'END1' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-    --nucnm_rate 1.0 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --mse_rate 0.1 \
-    --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
-    --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
+# python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+#     --noise_path $BASE_PATH'/data/DEMAND_16k' \
+#     --corruption_type 'END1' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
+#     --nucnm_rate 1.0 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --mse_rate 0.05 \
+#     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
+#     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
 # python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
 #     --noise_path $BASE_PATH'/data/DEMAND_16k' \
 #     --corruption_type 'END1' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 15 --lr '5e-5' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.05 \
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
@@ -21,7 +21,7 @@ python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/d
 #     --noise_path $BASE_PATH'/data/DEMAND_16k' \
 #     --corruption_type 'END2' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 \
+#     --nucnm_rate 1.0 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --mse_rate 0.05 \
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
@@ -29,7 +29,7 @@ python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/d
 #     --noise_path $BASE_PATH'/data/DEMAND_16k' \
 #     --corruption_type 'END2' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 15 --lr '5e-5' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 \
+#     --nucnm_rate 1.0 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --mse_rate 0.1 \
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
@@ -37,7 +37,7 @@ python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/d
 #     --noise_path $BASE_PATH'/data' \
 #     --corruption_type 'ENSC' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 15 --lr '5e-5' --aut_lr_decay 0.45 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.1 \
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
@@ -45,21 +45,21 @@ python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/d
 #     --noise_path $BASE_PATH'/data' \
 #     --corruption_type 'ENSC' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 15 --lr '5e-5' --aut_lr_decay 0.45 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 0.1 --gent_q 1.1 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 0.1 --gent_q 1.1 --mse_rate 0.05 \
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
 # python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
 #     --corruption_type 'WHN' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.05 \
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
 # python -m AuT.ReefSet.ttda --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
 #     --corruption_type 'WHN' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
 #     --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 \
+#     --nucnm_rate 1.0 --ent_rate 1.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.1 \
 #     --aut_wght_pth './result/ReefSet/AMAuT/train/aut-RS.pt' \
 #     --clsf_wght_pth './result/ReefSet/AMAuT/train/clsf-RS.pt' --wandb
 
