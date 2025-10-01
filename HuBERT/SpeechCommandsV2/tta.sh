6658ab48a30171e806d1ab3b7e9fca966fa54df3
Andy-Shao <hj13035952619@126.com>
2025-10-01 16:48:14 +0000

    optimization

--- HuBERT/SpeechCommandsV2/tta.sh
+++ HuBERT/SpeechCommandsV2/tta.sh
@@ -57,18 +57,18 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
 #     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
 
-python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-    --cache_path $BASE_PATH'/tmp'  --noise_path $BASE_PATH'/data/DEMAND_16k' \
-    --max_epoch 35 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
-    --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --lr_momentum 0.925 --mse_rate 0.5 \
-    --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'END1' --corruption_level 'L1' \
-    --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
-    --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
+# python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+#     --cache_path $BASE_PATH'/tmp'  --noise_path $BASE_PATH'/data/DEMAND_16k' \
+#     --max_epoch 35 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --lr_momentum 0.925 --mse_rate 0.5 \
+#     --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'END1' --corruption_level 'L1' \
+#     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
+#     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
 
 # python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
 #     --cache_path $BASE_PATH'/tmp'  --noise_path $BASE_PATH'/data/DEMAND_16k' \
 #     --max_epoch 35 --lr_cardinality 40 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
-#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 1 --lr_momentum 0.9 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 1 --lr_momentum 0.9 --mse_rate 0.6 \
 #     --ent_rate 0.25 --gent_rate 0.25 --gent_q 1.1 --corruption_type 'END2' --corruption_level 'L2' \
 #     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
 #     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
@@ -76,7 +76,7 @@ python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_ro
 # python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
 #     --cache_path $BASE_PATH'/tmp'  --noise_path $BASE_PATH'/data/DEMAND_16k' \
 #     --max_epoch 35 --lr_cardinality 40 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
-#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 1 --lr_momentum 0.9 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 1 --lr_momentum 0.9 --mse_rate 0.5 \
 #     --ent_rate 0.25 --gent_rate 0.25 --gent_q 1.1 --corruption_type 'END2' --corruption_level 'L1' \
 #     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
 #     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
@@ -84,7 +84,7 @@ python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_ro
 # python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
 #     --cache_path $BASE_PATH'/tmp' \
 #     --max_epoch 40 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
-#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --mse_rate 0.5 \
 #     --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'TST' --corruption_level 'L2' \
 #     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
 #     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
@@ -92,7 +92,7 @@ python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_ro
 # python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
 #     --cache_path $BASE_PATH'/tmp' \
 #     --max_epoch 40 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
-#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --mse_rate 0.5 \
 #     --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'TST' --corruption_level 'L1' \
 #     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
 #     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
@@ -100,15 +100,15 @@ python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_ro
 # python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
 #     --cache_path $BASE_PATH'/tmp'  --noise_path $BASE_PATH'/data/QUT-NOISE' \
 #     --max_epoch 40 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
-#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
+#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --mse_rate 0.1 \
 #     --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'ENQ' --corruption_level 'L2' \
 #     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
 #     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
 
-# python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-#     --cache_path $BASE_PATH'/tmp'  --noise_path $BASE_PATH'/data/QUT-NOISE' \
-#     --max_epoch 40 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
-#     --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 \
-#     --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'ENQ' --corruption_level 'L1' \
-#     --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
-#     --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
+python -m HuBERT.SpeechCommandsV2.ttda --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+    --cache_path $BASE_PATH'/tmp'  --noise_path $BASE_PATH'/data/QUT-NOISE' \
+    --max_epoch 40 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --hub_lr_decay 0.45 --num_workers 16 \
+    --nucnm_rate 1.0 --lr_gamma 30 --lr_threshold 35 --mse_rate 0.1 \
+    --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --corruption_type 'ENQ' --corruption_level 'L1' \
+    --hub_wght_pth './result/SpeechCommandsV2/HuBERT/train/hubert-base-SC2.pt' \
+    --clsf_wght_pth './result/SpeechCommandsV2/HuBERT/train/clsModel-base-SC2.pt' --wandb
