64e94ffd8a4c4bdf1f7d57b39f7b608ffcf9c3d7
Andy-Shao <hj13035952619@126.com>
2025-10-06 12:00:49 +0000

    add mse for HuV on VS

--- HuBERT/VocalSound/ttda.py
+++ HuBERT/VocalSound/ttda.py
@@ -15,7 +15,7 @@ from lib import constants
 from lib.spdataset import VocalSound
 from lib.dataset import batch_store_to, mlt_load_from, MultiTFDataset, mlt_store_to
 from lib.component import Components, AudioPadding, AudioClip, ReduceChannel, time_shift, DoNothing
-from lib.loss import entropy, g_entropy, nucnm
+from lib.loss import entropy, g_entropy, nucnm, mse
 from lib.lr_utils import build_optimizer, lr_scheduler
 from HuBERT.VocalSound.train import build_model, inference
 
@@ -113,6 +113,7 @@ if __name__ == '__main__':
     ap.add_argument('--ent_rate', type=float, default=1.)
     ap.add_argument('--gent_rate', type=float, default=1.)
     ap.add_argument('--gent_q', type=float, default=.9)
+    ap.add_argument('--mse_rate', type=float, default=0.0)
     ap.add_argument('--interval', type=int, default=1, help='interval number')
     ap.add_argument('--wandb', action='store_true')
     ap.add_argument('--seed', type=int, default=2025, help='random seed')
@@ -184,7 +185,7 @@ if __name__ == '__main__':
         print('Adaptating...')
         hubert.train(); clsf.train()
         ttl_size = 0.; ttl_loss = 0.; ttl_nucnm_loss = 0.
-        ttl_ent_loss = 0.; ttl_gent_loss = 0.
+        ttl_ent_loss = 0.; ttl_gent_loss = 0.; ttl_const_loss = 0.
         for fs1, fs2, _ in tqdm(adapt_loader):
             fs1, fs2 = fs1.to(args.device), fs2.to(args.device)
 
@@ -195,8 +196,9 @@ if __name__ == '__main__':
             nucnm_loss = nucnm(args, os1) + nucnm(args, os2)
             ent_loss = entropy(args, os1, epsilon=1e-8) + entropy(args, os2, epsilon=1e-8)
             gent_loss = g_entropy(args, os1, q=args.gent_q) + g_entropy(args, os2, q=args.gent_q)
+            const_loss = mse(args=args, out1=os1, out2=os2)
 
-            loss = nucnm_loss + ent_loss + gent_loss
+            loss = nucnm_loss + ent_loss + gent_loss + const_loss
             loss.backward()
             optimizer.step()
 
@@ -205,6 +207,7 @@ if __name__ == '__main__':
             ttl_nucnm_loss += nucnm_loss.cpu().item()
             ttl_ent_loss += ent_loss.cpu().item()
             ttl_gent_loss += gent_loss.cpu().item()
+            ttl_const_loss += const_loss.cpu().item()
 
         learning_rate = optimizer.param_groups[0]['lr']
         if epoch % args.interval == 0:
@@ -219,6 +222,7 @@ if __name__ == '__main__':
                 'Loss/Nuclear-norm loss': ttl_nucnm_loss / ttl_size,
                 'Loss/Entropy loss': ttl_ent_loss / ttl_size,
                 'Loss/G-entropy loss': ttl_gent_loss / ttl_size,
+                'Loss/Consistency loss': ttl_const_loss / ttl_size,
                 'Adaptation/accuracy': accuracy,
                 'Adaptation/LR': learning_rate,
                 'Adaptation/max_accu': max_accu,
@@ -232,6 +236,7 @@ if __name__ == '__main__':
             'Loss/Nuclear-norm loss': ttl_nucnm_loss / ttl_size,
             'Loss/Entropy loss': ttl_ent_loss / ttl_size,
             'Loss/G-entropy loss': ttl_gent_loss / ttl_size,
+            'Loss/Consistency loss': ttl_const_loss / ttl_size,
             'Adaptation/accuracy': accuracy,
             'Adaptation/LR': learning_rate,
             'Adaptation/max_accu': max_accu,
