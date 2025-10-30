8055fa6efa14bfe4f44ed7fdc28d7be2ce9b5f83
Andy-Shao <hj13035952619@126.com>
2025-10-30 07:16:48 +0000

    add processing

--- CoNMix/SpeechCommandsV2/STDA.sh
+++ CoNMix/SpeechCommandsV2/STDA.sh
@@ -99,6 +99,15 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --alpha 0.9 --initc_num 1 --cls_mode 'logsoft_nll' --lr_gamma 30 \
 #     --corruption_type 'ENSC' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --wandb 
 
+# python -m CoNMix.SpeechCommandsV2.STDA --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+#     --batch_size 32 --max_epoch 50 --interval 50 --early_stop 15 --lr '1e-4' --lr_momentum 0.75 \
+#     --modelF_weight_path './result/SpeechCommandsV2/CoNMix/train/CoN-SC2-modelF.pt' \
+#     --modelB_weight_path './result/SpeechCommandsV2/CoNMix/train/CoN-SC2-modelB.pt' \
+#     --modelC_weight_path './result/SpeechCommandsV2/CoNMix/train/CoN-SC2-modelC.pt' \
+#     --normalized --const_par 0.2 --fbnm_par 6.0 --cls_par 0.2 --plr 1 \
+#     --alpha 0.9 --initc_num 1 --cls_mode 'logsoft_nll' --lr_gamma 30 \
+#     --corruption_type 'TST' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --wandb 
+
 python -m CoNMix.SpeechCommandsV2.STDA --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
     --batch_size 32 --max_epoch 50 --interval 50 --early_stop 15 --lr '1e-4' --lr_momentum 0.75 \
     --modelF_weight_path './result/SpeechCommandsV2/CoNMix/train/CoN-SC2-modelF.pt' \
@@ -106,4 +115,4 @@ python -m CoNMix.SpeechCommandsV2.STDA --dataset 'SpeechCommandsV2' --dataset_ro
     --modelC_weight_path './result/SpeechCommandsV2/CoNMix/train/CoN-SC2-modelC.pt' \
     --normalized --const_par 0.2 --fbnm_par 6.0 --cls_par 0.2 --plr 1 \
     --alpha 0.9 --initc_num 1 --cls_mode 'logsoft_nll' --lr_gamma 30 \
-    --corruption_type 'TST' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --wandb 
+    --corruption_type 'TST' --corruption_level 'L2' --cache_path $BASE_PATH'/tmp' --wandb 
