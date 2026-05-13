07f7b501adada6239adb91153daa60248eeeb95b
Andy-Shao <hj13035952619@126.com>
2026-05-13 09:01:31 +0000

    optimization

--- AuT/UrbanSound8K/tta.sh
+++ AuT/UrbanSound8K/tta.sh
@@ -20,7 +20,7 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m AuT.UrbanSound8K.ttda --dataset 'UrbanSound8K' --dataset_root_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
     --corruption_type 'WHN' --corruption_level 'L1' --cache_path $BASE_PATH'/tmp' --batch_size 70 \
     --max_epoch 15 --lr '1e-4' --aut_lr_decay 0.55 --lr_momentum 0.70 \
-    --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 1.0 --gent_q 1.1 --mse_rate 0.1 \
+    --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 1.0 --gent_q 1.6 --mse_rate 1.0 \
     --aut_wght_pth './result/UrbanSound8K/AMAuT/train/aut-US8.pt' \
     --clsf_wght_pth './result/UrbanSound8K/AMAuT/train/clsf-US8.pt' --wandb
 
