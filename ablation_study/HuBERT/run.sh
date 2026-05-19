be0ba618fa5cce637af1c2461abd96729386304a
Andy-Shao <hj13035952619@126.com>
2026-05-19 09:22:04 +0000

    add ablation study

---
+++ ablation_study/HuBERT/run.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+python -m ablation_study.HuBERT.hub_fix_analysis --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.1 --mse_rate 0.1 \
+    --lr_momentum 0.75 \
+    --orig_wght_pth './result/SpeechCommandsV2/HuBERT/train' --wandb
