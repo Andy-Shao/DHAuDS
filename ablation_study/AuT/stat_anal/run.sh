1097b206aeed0dfe2d3559688d1d555eec65ccf4
Andy-Shao <hj13035952619@126.com>
2026-05-20 12:18:34 +0000

    optimization

--- ablation_study/AuT/stat_anal/run.sh
+++ ablation_study/AuT/stat_anal/run.sh
@@ -4,6 +4,6 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 # Experiment 01
 python -m ablation_study.AuT.aut_fix_analysis --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 1.0 \
+    --max_epoch 30 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 1.0 --gent_q 2.0 --mse_rate 1.0 \
     --lr_momentum 0.75 --aut_lr_decay 0.55 \
     --orig_wght_pth './result/SpeechCommandsV2/AMAuT/train' --wandb
