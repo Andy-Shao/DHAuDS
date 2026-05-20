781a64a2585ac1f499c31262a69e0f6fff6cd350
Andy-Shao <hj13035952619@126.com>
2026-05-20 00:25:23 +0000

    add stat_anal records

--- HuBERT/SpeechCommandsV2/stat_anal/stat_anal.sh
+++ HuBERT/SpeechCommandsV2/stat_anal/stat_anal.sh
@@ -3,7 +3,9 @@ export BASE_PATH=${BASE_PATH-:'/root'}
 report_list="\
 ./result/SpeechCommandsV2/HuBERT/Analysis/SpeechCommandsV2_analysis.csv, \
 ./result/SpeechCommandsV2/HuBERT/Analysis/SpeechCommandsV2_analysis02.csv, \
-./result/SpeechCommandsV2/HuBERT/Analysis/SpeechCommandsV2_analysis03.csv"
+./result/SpeechCommandsV2/HuBERT/Analysis/SpeechCommandsV2_analysis03.csv, \
+./result/SpeechCommandsV2/HuBERT/Analysis/SpeechCommandsV2_analysis04.csv, \
+./result/SpeechCommandsV2/HuBERT/Analysis/SpeechCommandsV2_analysis05.csv"
 
 python -m HuBERT.SpeechCommandsV2.stat_anal.stat_anal --output_file_name 'SC2-C_statistic_analysis.csv' \
     --report_list "${report_list}"
