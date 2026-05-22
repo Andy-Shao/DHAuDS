eab4cbe3803949a792f1aebf9a8d1fa2a32e8c9f
Andy-Shao <hj13035952619@126.com>
2026-05-22 10:16:53 +0000

    optimization

--- AuT/VocalSound/tta.sh
+++ AuT/VocalSound/tta.sh
@@ -7,7 +7,7 @@ python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_P
     --lr_gamma 10 --lr_threshold 15 --corruption_type 'WHN' --corruption_level 'L1' \
     --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.25 --gent_q 1.1 --mse_rate 0.1 \
     --aut_wght_pth './result/VocalSound/AMAuT/train/aut-VS.pt' \
-    --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt'
+    --clsf_wght_pth './result/VocalSound/AMAuT/train/clsf-VS.pt' --wandb
 
 # python -m AuT.VocalSound.ttda --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
 #     --cache_path $BASE_PATH'/tmp' \
