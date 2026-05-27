e8fc76e08d54499b224f0fd991cdb7969b113a0c
Andy-Shao <hj13035952619@126.com>
2026-05-27 18:55:57 +0000

    optimization

--- PANNs/UrbanSound8K/ttda.sh
+++ PANNs/UrbanSound8K/ttda.sh
@@ -28,6 +28,7 @@ python -m PANNs.UrbanSound8K.ttda --dataset 'UrbanSound8K' \
     --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
     --corruption_type 'ENSC' --corruption_level 'L1' --batch_size 70 --max_epoch 20 --lr 1e-4 \
     --nucnm_rate 1.0 --ent_rate 0.1 --gent_rate 0.1 --gent_q 1.6 --mse_rate 0.1 \
+    --freeze_pan \
     --orig_wght_pth './result/UrbanSound8K/PANNs/train' --wandb
 
 # python -m PANNs.UrbanSound8K.ttda --dataset 'UrbanSound8K' \
