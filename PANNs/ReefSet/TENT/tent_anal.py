f37b23cd5630b43318d4c6f2ceb55e6c47bbcbc4
Andy-Shao <hj13035952619@126.com>
2026-05-31 12:16:00 +0000

    add Tent on PAANs for RS-C

---
+++ PANNs/ReefSet/TENT/tent_anal.py
@@ -0,0 +1,119 @@
+import argparse
+import os
+import numpy as np
+import random
+import pandas as pd
+from tqdm import tqdm
+
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+from torchaudio.transforms import Resample
+
+from lib import constants
+from lib.utils import make_unless_exits, print_argparse, count_ttl_params
+from lib.corruption import corruption_meta, ReefSetC
+from lib.component import Components, AudioClip, AudioPadding, ReduceChannel, OneHot2Index
+from lib.tentAdapt import verify_model, get_params, TentAdapt
+from PANNs.lib.utils import build_model, load_weight, TentPANNs
+from ..utils import inference
+
+def analyzing(args:argparse.Namespace, corruption_types:list[str], corruption_levels:list[str]) -> None:
+    records = pd.DataFrame(columns=['Dataset',  'Algorithm', 'Param No.', 'Corruption', 'Non-adapted', 'Adapted', 'Improved'])
+    corruption_metas = corruption_meta(corruption_types=corruption_types, corruption_levels=corruption_levels)
+    pan, clsf = build_model(args, use_pre_weight=False)
+    param_no = count_ttl_params(pan) + count_ttl_params(clsf)
+
+    for idx, cmeta in enumerate(corruption_metas):
+        print(f'{idx+1}/{len(corruption_metas)}: {args.dataset} {cmeta.type}-{cmeta.level} analyzing...')
+
+        adpt_set = ReefSetC(
+            root_path=args.adpt_set_path, corruption_level=cmeta.level, corruption_type=cmeta.type,
+            data_tf=Components(transforms=[
+                Resample(orig_freq=args.sample_rate, new_freq=constants.pann_sample_rate),
+                AudioPadding(max_length=args.audio_length, sample_rate=constants.pann_sample_rate, random_shift=False),
+                AudioClip(max_length=args.audio_length, mode='head', is_random=False),
+                ReduceChannel()
+            ]),
+            label_tf=OneHot2Index()
+        )
+        adpt_loader = DataLoader(
+            dataset=adpt_set, batch_size=args.batch_size, shuffle=False, drop_last=False, pin_memory=True,
+            num_workers=args.num_workers
+        )
+        eval_set = ReefSetC(
+            root_path=args.eval_set_path, corruption_level=cmeta.level, corruption_type=cmeta.type,
+            data_tf=Components(transforms=[
+                Resample(orig_freq=args.sample_rate, new_freq=constants.pann_sample_rate),
+                AudioPadding(max_length=args.audio_length, sample_rate=constants.pann_sample_rate, random_shift=False),
+                AudioClip(max_length=args.audio_length, mode='head', is_random=False),
+                ReduceChannel()
+            ]),
+            label_tf=OneHot2Index()
+        )
+        eval_loader = DataLoader(
+            dataset=eval_set, batch_size=args.batch_size, shuffle=False, drop_last=False, pin_memory=True,
+            num_workers=args.num_workers
+        )
+
+        print('Non-adaptation analyzing...')
+        pan, clsf = build_model(args, use_pre_weight=False)
+        load_weight(args=args, panns=pan, clsf=clsf, mode='origin')
+        orig_roc_auc = inference(args=args, pan=pan, clsf=clsf, data_loader=eval_loader)
+
+        print('TENT adapting...')
+        tp_model = TentPANNs(pan=pan, clsf=clsf)
+        tent_params, tent_param_names = get_params(model=tp_model)
+        tent_optimizer = optim.Adam(params=tent_params, lr=args.lr, betas=(.9,.99), weight_decay=0.)
+        tent_model = TentAdapt(model=tp_model, optimizer=tent_optimizer, steps=1, resetable=False).to(device=args.device)
+        verify_model(model=tent_model)
+        for features, labels in tqdm(adpt_loader):
+            features = features.to(args.device)
+            tent_model(features)
+        print('Adaptation analyzing...')
+        adpt_roc_auc = inference(args=args, pan=pan, clsf=clsf, data_loader=eval_loader)
+        print(f'{args.dataset} {cmeta.type}-{cmeta.level} non-adapted accuracy: {orig_roc_auc:.4f}, adapted accuracy: {adpt_roc_auc:.4f}')
+        records.loc[len(records)] = [args.dataset, args.arch, param_no, f'{cmeta.type}-{cmeta.level}', orig_roc_auc, adpt_roc_auc, adpt_roc_auc - orig_roc_auc]
+    records.to_csv(os.path.join(args.output_path, args.output_file_name))
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--dataset', type=str, default='ReefSet', choices=['ReefSet'])
+    ap.add_argument('--adpt_set_path', type=str)
+    ap.add_argument('--eval_set_path', type=str)
+    ap.add_argument('--corruption_type', type=str, choices=['WHN', 'ENQ', 'END1', 'END2', 'ENSC', 'PSH', 'TST'])
+    ap.add_argument('--corruption_level', type=str, choices=['L1', 'L2'])
+    ap.add_argument('--num_workers', type=int, default=16)
+    ap.add_argument('--output_path', type=str, default='./result')
+    ap.add_argument('--output_file_name', type=str, default='analysis.csv')
+    ap.add_argument('--batch_size', type=int, default=64)
+
+    ap.add_argument('--lr', type=float, default=1e-2, help='learning rate')
+    ap.add_argument('--lr_momentum', type=float, default=.9)
+    ap.add_argument('--pan_lr_decay', type=float, default=1.0)
+    ap.add_argument('--clsf_lr_decay', type=float, default=1.0)
+
+    ap.add_argument('--seed', type=int, default=2025, help='random seed')
+    ap.add_argument('--orig_wght_pth', type=str)
+
+    args = ap.parse_args()
+    if args.dataset == 'ReefSet':
+        args.class_num = 37
+        args.sample_rate = 16000
+        args.audio_length = int(1.88 * constants.pann_sample_rate)
+    else:
+        raise Exception('No support!')
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.arch = 'PANNs'
+    args.output_path = os.path.join(args.output_path, args.dataset, args.arch, 'TENT')
+    make_unless_exits(args.output_path)
+    torch.backends.cudnn.benchmark = True
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    print_argparse(args)
+    ##########################################
+    analyzing(args=args, corruption_types=['WHN', 'ENQ', 'END1', 'END2', 'ENSC', 'PSH', 'TST'], corruption_levels=['L1', 'L2']) 
