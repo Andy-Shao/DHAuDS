d47043860ee30d7f103a9f9b056b6b875572465a
Andy-Shao <hj13035952619@126.com>
2026-06-01 10:20:40 +0000

    optimization

--- AuT/ReefSet/TTN/ttn_anal.sh
+++ AuT/ReefSet/TTN/ttn_anal.sh
@@ -1,9 +1,37 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m AuT.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C'\
+#     --eval_set_path $BASE_PATH'/data/ReefSet-C'\
+#     --batch_size 32 --output_file_name 'AuT_RS-C_TTN_analysis.csv' \
+#     --adpt_batch_size 70 --lr_momentum 0.1 \
+#     --orig_wght_pth './result/ReefSet/AMAuT/train'
+
 python -m AuT.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C'\
     --eval_set_path $BASE_PATH'/data/ReefSet-C'\
-    --batch_size 32 --output_file_name 'AuT_RS-C_TTN_analysis.csv' \
+    --batch_size 32 --output_file_name 'AuT_RS-C_TTN_analysis-02.csv' \
     --adpt_batch_size 70 --lr_momentum 0.1 \
-    --orig_wght_pth './result/ReefSet/AMAuT/train'
+    --orig_wght_pth './result/ReefSet/AMAuT/train' --seed 123456
+
+python -m AuT.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C'\
+    --eval_set_path $BASE_PATH'/data/ReefSet-C'\
+    --batch_size 32 --output_file_name 'AuT_RS-C_TTN_analysis-03.csv' \
+    --adpt_batch_size 70 --lr_momentum 0.1 \
+    --orig_wght_pth './result/ReefSet/AMAuT/train' --seed 654321
+
+python -m AuT.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C'\
+    --eval_set_path $BASE_PATH'/data/ReefSet-C'\
+    --batch_size 32 --output_file_name 'AuT_RS-C_TTN_analysis-04.csv' \
+    --adpt_batch_size 70 --lr_momentum 0.1 \
+    --orig_wght_pth './result/ReefSet/AMAuT/train' --seed 891011
+
+python -m AuT.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C'\
+    --eval_set_path $BASE_PATH'/data/ReefSet-C'\
+    --batch_size 32 --output_file_name 'AuT_RS-C_TTN_analysis-05.csv' \
+    --adpt_batch_size 70 --lr_momentum 0.1 \
+    --orig_wght_pth './result/ReefSet/AMAuT/train' --seed 111098
