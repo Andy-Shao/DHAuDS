10548df39f0b8a36a84aa968644797d5d3de09eb
Andy-Shao <hj13035952619@126.com>
2026-06-01 07:00:57 +0000

    add multi-ttn analysis

--- PANNs/ReefSet/TTN/ttn_anal.sh
+++ PANNs/ReefSet/TTN/ttn_anal.sh
@@ -1,9 +1,37 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m PANNs.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+#     --output_file_name 'PAN_RS-C_TTN_analysis.csv' --batch_size 32 \
+#     --lr 1e-5 --adpt_batch_size 70 \
+#     --orig_wght_pth './result/ReefSet/PANNs/train'
+
 python -m PANNs.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-    --output_file_name 'PAN_RS-C_TTN_analysis.csv' --batch_size 32 \
+    --output_file_name 'PAN_RS-C_TTN_analysis-02.csv' --batch_size 32 \
     --lr 1e-5 --adpt_batch_size 70 \
-    --orig_wght_pth './result/ReefSet/PANNs/train'
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 123456
+
+python -m PANNs.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --output_file_name 'PAN_RS-C_TTN_analysis-03.csv' --batch_size 32 \
+    --lr 1e-5 --adpt_batch_size 70 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 654321
+
+python -m PANNs.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --output_file_name 'PAN_RS-C_TTN_analysis-04.csv' --batch_size 32 \
+    --lr 1e-5 --adpt_batch_size 70 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 891011
+
+python -m PANNs.ReefSet.TTN.ttn_anal --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --output_file_name 'PAN_RS-C_TTN_analysis-05.csv' --batch_size 32 \
+    --lr 1e-5 --adpt_batch_size 70 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 111098
