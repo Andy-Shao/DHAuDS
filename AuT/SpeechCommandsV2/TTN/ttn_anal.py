a52d166f87de96f75f92b571d9d3668738614d64
Andy-Shao <hj13035952619@126.com>
2026-06-01 14:05:08 +0000

    add AuT TTN on SC2-C

---
+++ AuT/SpeechCommandsV2/TTN/ttn_anal.py
@@ -0,0 +1,122 @@
+
+import argparse
+import os
+import numpy as np
+import random
+import pandas as pd
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+from torchaudio.transforms import MelSpectrogram
+
+from lib.utils import make_unless_exits, print_argparse, count_ttl_params
+from lib.corruption import corruption_meta, SpeechCommandsV2C
+from lib.component import Components, AmplitudeToDB, FrequenceTokenTransformer
+from lib.normAdapt import NormAdapt
+from AuT.SpeechCommandsV2.train import build_model, inference
+from AuT.SpeechCommandsV2.utils import Sc2AuT
+from AuT.ReefSet.analysis import load_weight
+
+def analyzing(args:argparse.Namespace, corruption_types:list[str], corruption_levels:list[str]) -> None:
+    args.n_mels=80
+    n_fft=1024
+    win_length=400
+    hop_length=155
+    mel_scale='slaney'
+    args.target_length=104
+    records = pd.DataFrame(columns=['Dataset',  'Algorithm', 'Param No.', 'Corruption', 'Non-adapted', 'Adapted', 'Improved'])
+    corruption_metas = corruption_meta(corruption_types=corruption_types, corruption_levels=corruption_levels)
+    aut, clsf = build_model(args=args)
+    param_no = count_ttl_params(aut) + count_ttl_params(clsf)
+
+    for idx, cmeta in enumerate(corruption_metas):
+        print(f'{idx+1}/{len(corruption_metas)}: {args.dataset} {cmeta.type}-{cmeta.level} analyzing...')
+
+        adpt_set = SpeechCommandsV2C(
+            root_path=args.adpt_set_path, corruption_type=cmeta.type, corruption_level=cmeta.level,
+            data_tf=Components(transforms=[
+                MelSpectrogram(
+                    sample_rate=args.sample_rate, n_fft=n_fft, win_length=win_length, hop_length=hop_length,
+                    n_mels=args.n_mels, mel_scale=mel_scale
+                ),
+                AmplitudeToDB(top_db=80., max_out=2.),
+                FrequenceTokenTransformer()
+            ])
+        )
+        adpt_loader = DataLoader(
+            dataset=adpt_set, batch_size=args.adpt_batch_size, shuffle=True, drop_last=False, pin_memory=True,
+            num_workers=args.num_workers
+        )
+        eval_set = SpeechCommandsV2C(
+            root_path=args.eval_set_path, corruption_type=cmeta.type, corruption_level=cmeta.level,
+            data_tf=Components(transforms=[
+                MelSpectrogram(
+                    sample_rate=args.sample_rate, n_fft=n_fft, win_length=win_length, hop_length=hop_length,
+                    n_mels=args.n_mels, mel_scale=mel_scale
+                ),
+                AmplitudeToDB(top_db=80., max_out=2.),
+                FrequenceTokenTransformer()
+            ])
+        )
+        eval_loader = DataLoader(
+            dataset=eval_set, batch_size=args.batch_size, shuffle=False, drop_last=False, pin_memory=True,
+            num_workers=args.num_workers
+        )
+
+        print('Non-adaptation analyzing...')
+        aut, clsf = build_model(args=args)
+        load_weight(args=args, aut=aut, clsf=clsf, mode='origin')
+        orig_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=eval_loader)
+
+        print('TTN adapting...')
+        tp_model = Sc2AuT(aut=aut, clsf=clsf)
+        ttn_model = NormAdapt(model=tp_model, momentum=args.lr_momentum, reset_states=False).to(device=args.device)
+        for features, labels in tqdm(adpt_loader):
+            features = features.to(args.device)
+            ttn_model(features)
+        print('Adaptation analyzing...')
+        adpt_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=eval_loader)
+        print(f'{args.dataset} {cmeta.type}-{cmeta.level} non-adapted accuracy: {orig_accu:.4f}, adapted accuracy: {adpt_accu:.4f}')
+        records.loc[len(records)] = [args.dataset, args.arch, param_no, f'{cmeta.type}-{cmeta.level}', orig_accu, adpt_accu, adpt_accu - orig_accu]
+    records.to_csv(os.path.join(args.output_path, args.output_file_name))
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--dataset', type=str, default='SpeechCommandsV2', choices=['SpeechCommandsV2'])
+    ap.add_argument('--adpt_set_path', type=str)
+    ap.add_argument('--eval_set_path', type=str)
+    ap.add_argument('--num_workers', type=int, default=16)
+    ap.add_argument('--output_path', type=str, default='./result')
+    ap.add_argument('--output_file_name', type=str, default='analysis.csv')
+    ap.add_argument('--batch_size', type=int, default=64)
+    ap.add_argument('--adpt_batch_size', type=int, default=64)
+
+    ap.add_argument('--lr', type=float, default=1e-2, help='learning rate')
+    ap.add_argument('--lr_momentum', type=float, default=.9)
+    ap.add_argument('--pan_lr_decay', type=float, default=1.0)
+    ap.add_argument('--clsf_lr_decay', type=float, default=1.0)
+
+    ap.add_argument('--seed', type=int, default=2025, help='random seed')
+    ap.add_argument('--orig_wght_pth', type=str)
+
+    args = ap.parse_args()
+    if args.dataset == 'SpeechCommandsV2':
+        args.class_num = 35
+        args.sample_rate = 16000
+    else:
+        raise Exception('No support!')
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.arch = 'AMAuT'
+    args.output_path = os.path.join(args.output_path, args.dataset, args.arch, 'TTN')
+    make_unless_exits(args.output_path)
+    torch.backends.cudnn.benchmark = True
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    print_argparse(args)
+    ##########################################
+    analyzing(args=args, corruption_types=['WHN', 'ENQ', 'END1', 'END2', 'ENSC', 'PSH', 'TST'], corruption_levels=['L1', 'L2']) 
