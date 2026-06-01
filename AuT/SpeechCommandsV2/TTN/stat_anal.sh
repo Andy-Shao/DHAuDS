499916a39d5991aa898ee58e70394c82482fb064
Andy-Shao <hj13035952619@126.com>
2026-06-01 14:31:32 +0000

    add analysis for AuT on SC2-C

---
+++ AuT/SpeechCommandsV2/TTN/stat_anal.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/SpeechCommandsV2/AMAuT/TTN/AuT_SC2-C_TTN_analysis.csv, \
+./result/SpeechCommandsV2/AMAuT/TTN/AuT_SC2-C_TTN_analysis-02.csv, \
+./result/SpeechCommandsV2/AMAuT/TTN/AuT_SC2-C_TTN_analysis-03.csv, \
+./result/SpeechCommandsV2/AMAuT/TTN/AuT_SC2-C_TTN_analysis-04.csv, \
+./result/SpeechCommandsV2/AMAuT/TTN/AuT_SC2-C_TTN_analysis-05.csv"
+
+python -m AuT.SpeechCommandsV2.stat_anal.stat_anal --output_path 'TTN' \
+    --output_file_name 'AuT_SC2-C_TTN_static_analysis.csv' \
+    --report_list "${report_list}"
--- AuT/SpeechCommandsV2/TTN/ttn_anal.sh
+++ AuT/SpeechCommandsV2/TTN/ttn_anal.sh
@@ -1,9 +1,37 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m AuT.SpeechCommandsV2.TTN.ttn_anal --dataset 'SpeechCommandsV2' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+#     --output_file_name 'AuT_SC2-C_TTN_analysis.csv' --batch_size 32 \
+#     --adpt_batch_size 70 --lr_momentum 0.1 \
+#     --orig_wght_pth './result/SpeechCommandsV2/AMAuT/train'
+
 python -m AuT.SpeechCommandsV2.TTN.ttn_anal --dataset 'SpeechCommandsV2' \
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
-    --output_file_name 'AuT_SC2-C_TTN_analysis.csv' --batch_size 32 \
+    --output_file_name 'AuT_SC2-C_TTN_analysis-02.csv' --batch_size 32 \
     --adpt_batch_size 70 --lr_momentum 0.1 \
-    --orig_wght_pth './result/SpeechCommandsV2/AMAuT/train'
+    --orig_wght_pth './result/SpeechCommandsV2/AMAuT/train' --seed 123456
+
+python -m AuT.SpeechCommandsV2.TTN.ttn_anal --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --output_file_name 'AuT_SC2-C_TTN_analysis-03.csv' --batch_size 32 \
+    --adpt_batch_size 70 --lr_momentum 0.1 \
+    --orig_wght_pth './result/SpeechCommandsV2/AMAuT/train' --seed 654321
+
+python -m AuT.SpeechCommandsV2.TTN.ttn_anal --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --output_file_name 'AuT_SC2-C_TTN_analysis-04.csv' --batch_size 32 \
+    --adpt_batch_size 70 --lr_momentum 0.1 \
+    --orig_wght_pth './result/SpeechCommandsV2/AMAuT/train' --seed 891011
+
+python -m AuT.SpeechCommandsV2.TTN.ttn_anal --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --output_file_name 'AuT_SC2-C_TTN_analysis-05.csv' --batch_size 32 \
+    --adpt_batch_size 70 --lr_momentum 0.1 \
+    --orig_wght_pth './result/SpeechCommandsV2/AMAuT/train' --seed 111098
