cc47c217e7b97d0bd351da9d7978cae6a37f986b
Andy-Shao <hj13035952619@126.com>
2026-06-01 17:24:04 +0000

    add multi-analysis

--- PANNs/SpeechCommandsV2/TENT/tent_anal.sh
+++ PANNs/SpeechCommandsV2/TENT/tent_anal.sh
@@ -1,9 +1,37 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m PANNs.SpeechCommandsV2.TENT.tent_anal --dataset 'SpeechCommandsV2' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+#     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+#     --batch_size 32 --adpt_batch_size 70 --lr 1e-5 \
+#     --output_file_name 'PAN_SC2-C_TENT_analysis.csv' \
+#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train'
+
 python -m PANNs.SpeechCommandsV2.TENT.tent_anal --dataset 'SpeechCommandsV2' \
     --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
     --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
     --batch_size 32 --adpt_batch_size 70 --lr 1e-5 \
-    --output_file_name 'PAN_SC2-C_TENT_analysis.csv' \
-    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train'
+    --output_file_name 'PAN_SC2-C_TENT_analysis-02.csv' \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --seed 123456
+
+python -m PANNs.SpeechCommandsV2.TENT.tent_anal --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --batch_size 32 --adpt_batch_size 70 --lr 1e-5 \
+    --output_file_name 'PAN_SC2-C_TENT_analysis-03.csv' \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --seed 654321
+
+python -m PANNs.SpeechCommandsV2.TENT.tent_anal --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --batch_size 32 --adpt_batch_size 70 --lr 1e-5 \
+    --output_file_name 'PAN_SC2-C_TENT_analysis-04.csv' \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --seed 891011
+
+python -m PANNs.SpeechCommandsV2.TENT.tent_anal --dataset 'SpeechCommandsV2' \
+    --adpt_set_path $BASE_PATH'/data/Ada-SpeechCommandsV2-C' \
+    --eval_set_path $BASE_PATH'/data/SpeechCommandsV2-C' \
+    --batch_size 32 --adpt_batch_size 70 --lr 1e-5 \
+    --output_file_name 'PAN_SC2-C_TENT_analysis-05.csv' \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --seed 111098
