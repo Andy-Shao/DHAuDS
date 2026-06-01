87f1cc0e01cfd246a7754db123635fb74bee0184
Andy-Shao <hj13035952619@126.com>
2026-06-01 04:33:41 +0000

    add info

--- PANNs/ReefSet/TENT/tent_anal.sh
+++ PANNs/ReefSet/TENT/tent_anal.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
-# python -m PANNs.ReefSet.TENT.tent_anal --dataset 'ReefSet' \
-#     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
-#     --eval_set_path $BASE_PATH'/data/ReefSet-C' \
-#     --output_file_name 'PAN_RS-C_TENT_analysis.csv' --batch_size 32 \
-#     --lr 1e-5 --adpt_batch_size 70 \
-#     --orig_wght_pth './result/ReefSet/PANNs/train'
+python -m PANNs.ReefSet.TENT.tent_anal --dataset 'ReefSet' \
+    --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
+    --eval_set_path $BASE_PATH'/data/ReefSet-C' \
+    --output_file_name 'PAN_RS-C_TENT_analysis.csv' --batch_size 32 \
+    --lr 1e-5 --adpt_batch_size 70 \
+    --orig_wght_pth './result/ReefSet/PANNs/train'
 
 python -m PANNs.ReefSet.TENT.tent_anal --dataset 'ReefSet' \
     --adpt_set_path $BASE_PATH'/data/Ada-ReefSet-C' \
