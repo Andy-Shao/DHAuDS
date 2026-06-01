0edd87a56db0bd8d9e1c799758fc778d3c16e085
Andy-Shao <hj13035952619@126.com>
2026-06-01 18:17:21 +0000

    add analysis

---
+++ PANNs/SpeechCommandsV2/TENT/stat_anal.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/SpeechCommandsV2/PANNs/TENT/PAN_SC2-C_TENT_analysis.csv, \
+./result/SpeechCommandsV2/PANNs/TENT/PAN_SC2-C_TENT_analysis-02.csv, \
+./result/SpeechCommandsV2/PANNs/TENT/PAN_SC2-C_TENT_analysis-03.csv, \
+./result/SpeechCommandsV2/PANNs/TENT/PAN_SC2-C_TENT_analysis-04.csv, \
+./result/SpeechCommandsV2/PANNs/TENT/PAN_SC2-C_TENT_analysis-05.csv"
+
+python -m PANNs.SpeechCommandsV2.stat_anal.stat_anal --output_path 'TENT' \
+    --output_file_name 'PAN_SC2-C_TENT_static_analysis.csv' \
+    --report_list "${report_list}"
