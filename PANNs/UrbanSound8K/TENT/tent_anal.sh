46ddf256016a684b36cfb046000f29f93eb45a1f
Andy-Shao <hj13035952619@126.com>
2026-06-03 09:36:41 +0000

    add analysis

---
+++ PANNs/UrbanSound8K/TENT/tent_anal.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+python -m PANNs.UrbanSound8K.TENT.tent_anal --dataset 'UrbanSound8K' \
+    --adpt_set_path $BASE_PATH'/data/Ada-UrbanSound8K-C' \
+    --eval_set_path $BASE_PATH'/data/UrbanSound8K-C' \
+    --batch_size 32 --adpt_batch_size 70 --lr_momentum 0.1 \
+    --output_file_name 'PAN_US8-C_TENT_analysis.csv' \
+    --orig_wght_pth './result/UrbanSound8K/PANNs/train'
