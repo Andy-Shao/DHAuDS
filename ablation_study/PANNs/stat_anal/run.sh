d721b60b2e8b908ac750f335cf897672d61eaf2e
Andy-Shao <hj13035952619@126.com>
2026-06-03 15:26:24 +0000

    add analysis

--- ablation_study/PANNs/stat_anal/run.sh
+++ ablation_study/PANNs/stat_anal/run.sh
@@ -2,7 +2,13 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 # Experiment 01
-python -m ablation_study.PANNs.pan_fix_analysis --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
-    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 23 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.1 --gent_q 2.0 --mse_rate 1.0 \
-    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
+# python -m ablation_study.PANNs.pan_fix_analysis --dataset 'SpeechCommandsV2' --dataset_root_path $BASE_PATH'/data' \
+#     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 23 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.1 --gent_q 2.0 --mse_rate 1.0 \
+#     --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' --wandb
+
+python -m ablation_study.PANNs.stat_anal.analysis --dataset 'SpeechCommandsV2' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' \
+    --output_file_name 'fix-corruption_analysis.csv' --batch_size 32 \
+    --orig_wght_pth './result/SpeechCommandsV2/PANNs/train' \
+    --adpt_wght_pth $BASE_PATH'/tmp'
