dc100d59ef157f8566a05f59d688674a56d7a4ca
Andy-Shao <hj13035952619@126.com>
2026-06-03 11:43:50 +0000

    add evaluation

---
+++ AuT/VocalSound/TENT/tent_anal.py
@@ -0,0 +1,127 @@
+import argparse
+import os
+import numpy as np
+import random
+import pandas as pd
+from tqdm import tqdm
+
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+from torchaudio.transforms import MelSpectrogram
+
+from lib.utils import make_unless_exits, print_argparse, count_ttl_params
+from lib.corruption import corruption_meta, VocalSoundC
+from lib.component import Components, AmplitudeToDB, FrequenceTokenTransformer, MelSpectrogramPadding
+from lib.tentAdapt import TentAdapt, get_params, verify_model
+from AuT.VocalSound.train import build_model, inference
+from AuT.VocalSound.utils import VsAuT
+from AuT.ReefSet.analysis import load_weight
+
+def analyzing(args:argparse.Namespace, corruption_types:list[str], corruption_levels:list[str]) -> None:
+    args.n_mels=64
+    n_fft=1024
+    win_length=400
+    hop_length=154
+    mel_scale='slaney'
+    args.target_length=1040
+    records = pd.DataFrame(columns=['Dataset',  'Algorithm', 'Param No.', 'Corruption', 'Non-adapted', 'Adapted', 'Improved'])
+    corruption_metas = corruption_meta(corruption_types=corruption_types, corruption_levels=corruption_levels)
+    aut, clsf = build_model(args=args)
+    param_no = count_ttl_params(aut) + count_ttl_params(clsf)
+
+    for idx, cmeta in enumerate(corruption_metas):
+        print(f'{idx+1}/{len(corruption_metas)}: {args.dataset} {cmeta.type}-{cmeta.level} analyzing...')
+
+        adpt_set = VocalSoundC(
+            root_path=args.adpt_set_path, corruption_level=cmeta.level, corruption_type=cmeta.type,
+            data_tf=Components(transforms=[
+                MelSpectrogram(
+                    sample_rate=args.sample_rate, n_fft=n_fft, win_length=win_length, hop_length=hop_length,
+                    mel_scale=mel_scale, n_mels=args.n_mels
+                ),
+                AmplitudeToDB(top_db=80., max_out=2.),
+                MelSpectrogramPadding(target_length=args.target_length),
+                FrequenceTokenTransformer()
+            ])
+        )
+        adpt_loader = DataLoader(
+            dataset=adpt_set, batch_size=args.adpt_batch_size, shuffle=True, drop_last=False, pin_memory=True,
+            num_workers=args.num_workers
+        )
+        eval_set = VocalSoundC(
+            root_path=args.eval_set_path, corruption_level=cmeta.level, corruption_type=cmeta.type,
+            data_tf=Components(transforms=[
+                MelSpectrogram(
+                    sample_rate=args.sample_rate, n_fft=n_fft, win_length=win_length, hop_length=hop_length,
+                    mel_scale=mel_scale, n_mels=args.n_mels
+                ),
+                AmplitudeToDB(top_db=80., max_out=2.),
+                MelSpectrogramPadding(target_length=args.target_length),
+                FrequenceTokenTransformer()
+            ])
+        )
+        eval_loader = DataLoader(
+            dataset=eval_set, batch_size=args.batch_size, shuffle=False, drop_last=False, pin_memory=True,
+            num_workers=args.num_workers
+        )
+
+        print('Non-adaptation analyzing...')
+        aut, clsf = build_model(args=args)
+        load_weight(args=args, aut=aut, clsf=clsf, mode='origin')
+        orig_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=eval_loader)
+
+        print('TENT adapting...')
+        tp_model = VsAuT(aut=aut, clsf=clsf)
+        tent_params, tent_param_names = get_params(model=tp_model)
+        tent_optimizer = optim.Adam(params=tent_params, lr=args.lr, betas=(.9,.99), weight_decay=0.)
+        tent_model = TentAdapt(model=tp_model, optimizer=tent_optimizer, steps=1, resetable=False).to(device=args.device)
+        for features, labels in tqdm(adpt_loader):
+            features = features.to(args.device)
+            tent_model(features)
+        print('Adaptation analyzing...')
+        adpt_accu = inference(args=args, aut=aut, clsf=clsf, data_loader=eval_loader)
+        print(f'{args.dataset} {cmeta.type}-{cmeta.level} non-adapted accuracy: {orig_accu:.4f}, adapted accuracy: {adpt_accu:.4f}')
+        records.loc[len(records)] = [args.dataset, args.arch, param_no, f'{cmeta.type}-{cmeta.level}', orig_accu, adpt_accu, adpt_accu - orig_accu]
+    records.to_csv(os.path.join(args.output_path, args.output_file_name))
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--dataset', type=str, default='VocalSound', choices=['VocalSound'])
+    ap.add_argument('--adpt_set_path', type=str)
+    ap.add_argument('--eval_set_path', type=str)
+    ap.add_argument('--num_workers', type=int, default=16)
+    ap.add_argument('--output_path', type=str, default='./result')
+    ap.add_argument('--output_file_name', type=str, default='analysis.csv')
+    ap.add_argument('--batch_size', type=int, default=64)
+    ap.add_argument('--adpt_batch_size', type=int, default=64)
+
+    ap.add_argument('--lr', type=float, default=1e-2, help='learning rate')
+    ap.add_argument('--lr_momentum', type=float, default=.9)
+    ap.add_argument('--pan_lr_decay', type=float, default=1.0)
+    ap.add_argument('--clsf_lr_decay', type=float, default=1.0)
+
+    ap.add_argument('--seed', type=int, default=2025, help='random seed')
+    ap.add_argument('--orig_wght_pth', type=str)
+
+    args = ap.parse_args()
+    if args.dataset == 'VocalSound':
+        args.class_num = 6
+        args.sample_rate = 16000
+        args.audio_length = int(10 * args.sample_rate)
+    else:
+        raise Exception('No support!')
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.arch = 'AMAuT'
+    args.output_path = os.path.join(args.output_path, args.dataset, args.arch, 'TENT')
+    make_unless_exits(args.output_path)
+    torch.backends.cudnn.benchmark = True
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    print_argparse(args)
+    ##########################################
+    analyzing(args=args, corruption_types=['WHN', 'ENQ', 'END1', 'END2', 'ENSC', 'PSH', 'TST'], corruption_levels=['L1', 'L2']) 
---
+++ AuT/VocalSound/TENT/tent_anal.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+python -m AuT.VocalSound.TENT.tent_anal --dataset 'VocalSound' \
+    --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+    --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+    --output_file_name 'AuT_VS-C_TENT_analsysis.csv' --batch_size 32 \
+    --adpt_batch_size 70 --lr 1e-3 \
+    --orig_wght_pth './result/VocalSound/AMAuT/train'
