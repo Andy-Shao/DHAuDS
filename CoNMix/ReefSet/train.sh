faf1182e14ddf32e4c239f59a2b644caf56bb88f
Andy-Shao <hj13035952619@126.com>
2026-06-03 11:54:55 +0000

    omit CoNMix

--- CoNMix/ReefSet/train.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-export BASE_PATH=${BASE_PATH:-'/root'}
-
-python -m CoNMix.ReefSet.train --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
-    --max_epoch 15 --interval 15 --batch_size 63 --trte full --normalized --num_workers 16 --wandb
