88f1661d3f86f04c17e9cdeff92f494aa82a728f
Andy-Shao <hj13035952619@126.com>
2026-06-04 19:33:55 +0000

    optimization

--- ablation_study/HuBERT/ReefSet/stat_anal/run.sh
+++ ablation_study/HuBERT/ReefSet/stat_anal/run.sh
@@ -2,8 +2,14 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 # Experiment 01
-python -m ablation_study.HuBERT.ReefSet.hub_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
-    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 10 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.1 \
-    --lr_momentum 0.75 --hub_lr_decay 0.55 \
-    --orig_wght_pth './result/ReefSet/HuBERT/train' --wandb
+# python -m ablation_study.HuBERT.ReefSet.hub_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+#     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 10 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.1 \
+#     --lr_momentum 0.75 --hub_lr_decay 0.55 \
+#     --orig_wght_pth './result/ReefSet/HuBERT/train' --wandb
+
+python -m ablation_study.HuBERT.ReefSet.stat_anal.analysis --dataset 'ReefSet' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' --output_path './result' \
+    --batch_size 32 --model_level 'base' --output_file_name 'fix-corruption_analysis.csv' \
+    --orig_wght_pth './result/ReefSet/HuBERT/train' \
+    --adpt_wght_path $BASE_PATH'/tmp'
