066b3acc80d0ffe2c1d1fc31b0bfdd9fabc1a4e9
Andy-Shao <hj13035952619@126.com>
2026-06-04 07:39:26 +0000

    add static analaysis

---
+++ PANNs/VocalSound/stat_anal/stat_anal.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+report_list="\
+./result/VocalSound/PANNs/Analysis/PAN_VS-C_analysis.csv, \
+./result/VocalSound/PANNs/Analysis/PAN_VS-C_analysis-02.csv, \
+./result/VocalSound/PANNs/Analysis/PAN_VS-C_analysis-03.csv, \
+./result/VocalSound/PANNs/Analysis/PAN_VS-C_analysis-04.csv, \
+./result/VocalSound/PANNs/Analysis/PAN_VS-C_analysis-05.csv"
+
+python -m PANNs.VocalSound.stat_anal.stat_anal --output_file_name 'PAN_VS-C_statistic_analysis.csv' \
+    --report_list "${report_list}"
