dbf450fd54b0dc8eb69ee7cb8a337ba62b7e433f
Andy-Shao <hj13035952619@126.com>
2026-06-04 08:46:30 +0000

    add analysis

--- PANNs/VocalSound/TTN/ttn_anal.sh
+++ PANNs/VocalSound/TTN/ttn_anal.sh
@@ -1,9 +1,37 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
+# python -m PANNs.VocalSound.TTN.ttn_anal --dataset 'VocalSound' \
+#     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+#     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+#     --batch_size 32 --adpt_batch_size 70 --lr_momentum 0.1 \
+#     --output_file_name 'PAN_VS-C_TTN_analysis.csv' \
+#     --orig_wght_pth './result/VocalSound/PANNs/train'
+
 python -m PANNs.VocalSound.TTN.ttn_anal --dataset 'VocalSound' \
     --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
     --eval_set_path $BASE_PATH'/data/VocalSound-C' \
     --batch_size 32 --adpt_batch_size 70 --lr_momentum 0.1 \
-    --output_file_name 'PAN_VS-C_TTN_analysis.csv' \
-    --orig_wght_pth './result/VocalSound/PANNs/train'
+    --output_file_name 'PAN_VS-C_TTN_analysis-02.csv' \
+    --orig_wght_pth './result/VocalSound/PANNs/train' --seed 123456
+
+python -m PANNs.VocalSound.TTN.ttn_anal --dataset 'VocalSound' \
+    --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+    --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+    --batch_size 32 --adpt_batch_size 70 --lr_momentum 0.1 \
+    --output_file_name 'PAN_VS-C_TTN_analysis-03.csv' \
+    --orig_wght_pth './result/VocalSound/PANNs/train' --seed 654321
+
+python -m PANNs.VocalSound.TTN.ttn_anal --dataset 'VocalSound' \
+    --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+    --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+    --batch_size 32 --adpt_batch_size 70 --lr_momentum 0.1 \
+    --output_file_name 'PAN_VS-C_TTN_analysis-04.csv' \
+    --orig_wght_pth './result/VocalSound/PANNs/train' --seed 891011
+
+python -m PANNs.VocalSound.TTN.ttn_anal --dataset 'VocalSound' \
+    --adpt_set_path $BASE_PATH'/data/Ada-VocalSound-C' \
+    --eval_set_path $BASE_PATH'/data/VocalSound-C' \
+    --batch_size 32 --adpt_batch_size 70 --lr_momentum 0.1 \
+    --output_file_name 'PAN_VS-C_TTN_analysis-05.csv' \
+    --orig_wght_pth './result/VocalSound/PANNs/train' --seed 111098
