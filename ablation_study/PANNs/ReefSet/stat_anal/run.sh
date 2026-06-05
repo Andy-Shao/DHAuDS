1d8c60b82d416adea270d2a0ac46df50b19b0705
Andy-Shao <hj13035952619@126.com>
2026-06-05 08:43:35 +0000

    add multiple-analysis

--- ablation_study/PANNs/ReefSet/stat_anal/run.sh
+++ ablation_study/PANNs/ReefSet/stat_anal/run.sh
@@ -8,8 +8,60 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --lr_momentum 0.75 --pan_lr_decay 0.55 \
 #     --orig_wght_pth './result/ReefSet/PANNs/train' --wandb
 
+# python -m ablation_study.PANNs.ReefSet.stat_anal.analysis --dataset 'ReefSet' \
+#     --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' \
+#     --output_file_name 'fix-corruption_analysis.csv' --batch_size 32 \
+#     --orig_wght_pth './result/ReefSet/PANNs/train' \
+#     --adpt_wght_pth $BASE_PATH'/tmp'
+
+# Experiment 02
+python -m ablation_study.PANNs.ReefSet.pan_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 10 --lr 5e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --lr_momentum 0.75 --pan_lr_decay 0.55 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 123456
+
 python -m ablation_study.PANNs.ReefSet.stat_anal.analysis --dataset 'ReefSet' \
     --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' \
-    --output_file_name 'fix-corruption_analysis.csv' --batch_size 32 \
+    --output_file_name 'fix-corruption_analysis02.csv' --batch_size 32 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' \
+    --adpt_wght_pth $BASE_PATH'/tmp'
+
+# Experiment 03
+python -m ablation_study.PANNs.ReefSet.pan_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 10 --lr 5e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --lr_momentum 0.75 --pan_lr_decay 0.55 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 654321
+
+python -m ablation_study.PANNs.ReefSet.stat_anal.analysis --dataset 'ReefSet' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' \
+    --output_file_name 'fix-corruption_analysis03.csv' --batch_size 32 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' \
+    --adpt_wght_pth $BASE_PATH'/tmp'
+
+# Experiment 04
+python -m ablation_study.PANNs.ReefSet.pan_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 10 --lr 5e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --lr_momentum 0.75 --pan_lr_decay 0.55 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 891011
+
+python -m ablation_study.PANNs.ReefSet.stat_anal.analysis --dataset 'ReefSet' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' \
+    --output_file_name 'fix-corruption_analysis04.csv' --batch_size 32 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' \
+    --adpt_wght_pth $BASE_PATH'/tmp'
+
+# Experiment 05
+python -m ablation_study.PANNs.ReefSet.pan_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 10 --lr 5e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --lr_momentum 0.75 --pan_lr_decay 0.55 \
+    --orig_wght_pth './result/ReefSet/PANNs/train' --seed 111098
+
+python -m ablation_study.PANNs.ReefSet.stat_anal.analysis --dataset 'ReefSet' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' \
+    --output_file_name 'fix-corruption_analysis05.csv' --batch_size 32 \
     --orig_wght_pth './result/ReefSet/PANNs/train' \
     --adpt_wght_pth $BASE_PATH'/tmp'
