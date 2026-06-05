6165a0e5a608d96c486cf0d133cc9e524506da73
Andy-Shao <hj13035952619@126.com>
2026-06-05 16:04:05 +0000

    add analysis

---
+++ ablation_study/HuBERT/VocalSound/stat_anal/analysis.py
@@ -0,0 +1,70 @@
+import argparse
+import os
+import pandas as pd
+
+import torch
+from torch.utils.data import DataLoader
+
+from lib import constants
+from lib.utils import print_argparse, make_unless_exits
+from lib.dataset import mlt_load_from, MultiTFDataset
+from lib.component import Components, ReduceChannel, AudioPadding
+from HuBERT.VocalSound.train import build_model, inference
+from HuBERT.ReefSet.analysis import load_weight
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--dataset', type=str, default='VocalSound', choices=['VocalSound'])
+    ap.add_argument('--dataset_root_path', type=str)
+    ap.add_argument('--num_workers', type=int, default=16)
+    ap.add_argument('--output_path', type=str)
+    ap.add_argument('--batch_size', type=int, default=64, help='batch size')
+    ap.add_argument('--model_level', type=str, default='base', choices=['base', 'large', 'x-large'])
+    ap.add_argument('--use_pre_trained_weigth', action='store_true')
+    ap.add_argument('--orig_wght_pth', type=str)
+    ap.add_argument('--adpt_wght_path', type=str)
+    ap.add_argument('--output_file_name', type=str, default='analysis.csv')
+
+    args = ap.parse_args()
+    if args.dataset == 'VocalSound':
+        args.class_num = 6
+        args.sample_rate = 16000
+    else:
+        raise Exception('No support!')
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch.backends.cudnn.benchmark = True
+    args.arch = 'HuBERT'
+    args.output_path = os.path.join(args.output_path, args.dataset, args.arch, 'Ablation_study')
+
+    print_argparse(args)
+    make_unless_exits(args.output_path)
+    ##########################################
+    hubert, clsf = build_model(args=args, pre_weight=args.use_pre_trained_weigth)
+    records = pd.DataFrame(columns=['dataset', 'algorithm', 'type', 'before-adaptation', 'after-adaptation'])
+
+    noise_set = mlt_load_from(
+        root_path=args.dataset_root_path, index_file_name='meta_info.csv', class_num=args.class_num,
+    )
+
+    test_loader = DataLoader(
+        batch_size=args.batch_size, shuffle=False, drop_last=False,
+        num_workers=args.num_workers, 
+        dataset=MultiTFDataset(dataset=noise_set, tfs=[ReduceChannel()])
+    )
+
+    print('Before adaptation analyzing...')
+    load_weight(args=args, hubert=hubert, clsf=clsf, mode='origin')
+    b_f1 = inference(args=args, hubert=hubert, clsModel=clsf, data_loader=test_loader)
+    print(f'Before adaptation F1 score is: {b_f1:.4f}, sample size is: {len(noise_set)}')
+
+    print('After adaptation analyzing...')
+    hub_wght_pth = os.path.join(args.adpt_wght_path, f'hubert-{args.model_level}-{constants.dataset_dic[args.dataset]}-fix.pt')
+    hubert.load_state_dict(state_dict=torch.load(hub_wght_pth, weights_only=True))
+    clsf_wght_pth = os.path.join(args.adpt_wght_path, f'clsModel-{args.model_level}-{constants.dataset_dic[args.dataset]}-fix.pt')
+    clsf.load_state_dict(state_dict=torch.load(clsf_wght_pth, weights_only=True))
+    a_f1 = inference(args=args, hubert=hubert, clsModel=clsf, data_loader=test_loader)
+    print(f'After adaptation F1 score is: {a_f1:.4f}, sample size is: {len(noise_set)}')
+
+    records.loc[len(records)] = [args.dataset, args.arch, 'Fix-corruption', b_f1, a_f1]
+    records.to_csv(os.path.join(args.output_path, args.output_file_name))
+    print('END!')
--- ablation_study/HuBERT/VocalSound/stat_anal/run.sh
+++ ablation_study/HuBERT/VocalSound/stat_anal/run.sh
@@ -2,8 +2,14 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 # Experiment 01
-python -m ablation_study.HuBERT.VocalSound.hub_fix_analysis --dataset 'VocalSound' \
-    --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
-    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 15 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.5 \
-    --orig_wght_pth './result/VocalSound/HuBERT/train' --wandb
+# python -m ablation_study.HuBERT.VocalSound.hub_fix_analysis --dataset 'VocalSound' \
+#     --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+#     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 15 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 1.6 --mse_rate 0.5 \
+#     --orig_wght_pth './result/VocalSound/HuBERT/train' --wandb
+
+python -m ablation_study.HuBERT.VocalSound.stat_anal.analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' --output_path './result' \
+    --batch_size 32 --model_level 'base' --output_file_name 'fix-corruption_analysis.csv' \
+    --orig_wght_pth './result/VocalSound/HuBERT/train' \
+    --adpt_wght_path $BASE_PATH'/tmp'
