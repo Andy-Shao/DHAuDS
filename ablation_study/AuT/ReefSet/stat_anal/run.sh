390fe3ab90de16bc3389c1500a57e628bbfe916b
Andy-Shao <hj13035952619@126.com>
2026-06-05 11:17:49 +0000

    optimization

--- ablation_study/AuT/ReefSet/stat_anal/run.sh
+++ ablation_study/AuT/ReefSet/stat_anal/run.sh
@@ -5,4 +5,5 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m ablation_study.AuT.ReefSet.aut_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
     --max_epoch 10 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --lr_momentum 0.75 \
     --orig_wght_pth './result/ReefSet/AMAuT/train' --wandb
