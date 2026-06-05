24f88cd08800aeb8b5c43a26d4c63237167d35a5
Andy-Shao <hj13035952619@126.com>
2026-06-05 08:07:50 +0000

    optimization

--- ablation_study/PANNs/ReefSet/run.sh
+++ ablation_study/PANNs/ReefSet/run.sh
@@ -4,6 +4,6 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 # Experiment 01
 python -m ablation_study.PANNs.ReefSet.pan_fix_analysis --dataset 'ReefSet' --dataset_root_path $BASE_PATH'/data/ReefSet_v1.0' \
     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --max_epoch 10 --lr 5e-5 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
     --lr_momentum 0.75 --pan_lr_decay 0.55 \
     --orig_wght_pth './result/ReefSet/PANNs/train' --wandb
