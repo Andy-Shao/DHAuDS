0f7ae8f8237f950d1d1e305433346b9d8b2f170a
Andy-Shao <hj13035952619@126.com>
2026-06-06 10:08:23 +0000

    optimization

--- ablation_study/PANNs/VocalSound/stat_anal/run.sh
+++ ablation_study/PANNs/VocalSound/stat_anal/run.sh
@@ -6,4 +6,4 @@ python -m ablation_study.PANNs.VocalSound.pan_fix_analysis --dataset 'VocalSound
     --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
     --max_epoch 10 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
-    --orig_wght_pth './result/VocalSound/PANNs/train' 
+    --orig_wght_pth './result/VocalSound/PANNs/train' --freeze_pan --wandb
