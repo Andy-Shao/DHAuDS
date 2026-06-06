6297ecbfef917162c4527f8d4dce2172ea674164
Andy-Shao <hj13035952619@126.com>
2026-06-06 11:12:11 +0000

    add optimization

--- ablation_study/PANNs/VocalSound/stat_anal/run.sh
+++ ablation_study/PANNs/VocalSound/stat_anal/run.sh
@@ -2,8 +2,14 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 # Experiment 01
-python -m ablation_study.PANNs.VocalSound.pan_fix_analysis --dataset 'VocalSound' \
-    --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
-    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
-    --max_epoch 15 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
-    --orig_wght_pth './result/VocalSound/PANNs/train' --freeze_pan --wandb
+# python -m ablation_study.PANNs.VocalSound.pan_fix_analysis --dataset 'VocalSound' \
+#     --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+#     --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+#     --max_epoch 15 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+#     --orig_wght_pth './result/VocalSound/PANNs/train' --freeze_pan --wandb
+
+python -m ablation_study.PANNs.VocalSound.stat_anal.analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' \
+    --output_file_name 'fix-corruption_analysis.csv' --batch_size 32 \
+    --orig_wght_pth './result/VocalSound/PANNs/train' \
+    --adpt_wght_pth $BASE_PATH'/tmp'
