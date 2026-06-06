b6a255142f258b2979cc70b560dbc53cead36dd3
Andy-Shao <hj13035952619@126.com>
2026-06-06 13:33:27 +0000

    add analysis

--- ablation_study/AuT/VocalSound/stat_anal/run.sh
+++ ablation_study/AuT/VocalSound/stat_anal/run.sh
@@ -8,8 +8,60 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
 #     --orig_wght_pth './result/VocalSound/AMAuT/train' --wandb
 
+# python -m ablation_study.AuT.VocalSound.stat_anal.analysis --dataset 'VocalSound' \
+#     --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' --output_path './result' \
+#     --batch_size 32 --output_file_name 'fix-corruption_analysis.csv' \
+#     --orig_wght_pth './result/VocalSound/AMAuT/train' \
+#     --adpt_wght_path $BASE_PATH'/tmp'
+
+# Experiment 02
+python -m ablation_study.AuT.VocalSound.aut_fix_analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --orig_wght_pth './result/VocalSound/AMAuT/train' --seed 123456
+
 python -m ablation_study.AuT.VocalSound.stat_anal.analysis --dataset 'VocalSound' \
     --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' --output_path './result' \
-    --batch_size 32 --output_file_name 'fix-corruption_analysis.csv' \
+    --batch_size 32 --output_file_name 'fix-corruption_analysis02.csv' \
+    --orig_wght_pth './result/VocalSound/AMAuT/train' \
+    --adpt_wght_path $BASE_PATH'/tmp'
+
+# Experiment 03
+python -m ablation_study.AuT.VocalSound.aut_fix_analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --orig_wght_pth './result/VocalSound/AMAuT/train' --seed 654321
+
+python -m ablation_study.AuT.VocalSound.stat_anal.analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' --output_path './result' \
+    --batch_size 32 --output_file_name 'fix-corruption_analysis03.csv' \
+    --orig_wght_pth './result/VocalSound/AMAuT/train' \
+    --adpt_wght_path $BASE_PATH'/tmp'
+
+# Experiment 04
+python -m ablation_study.AuT.VocalSound.aut_fix_analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --orig_wght_pth './result/VocalSound/AMAuT/train' --seed 891011
+
+python -m ablation_study.AuT.VocalSound.stat_anal.analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' --output_path './result' \
+    --batch_size 32 --output_file_name 'fix-corruption_analysis04.csv' \
+    --orig_wght_pth './result/VocalSound/AMAuT/train' \
+    --adpt_wght_path $BASE_PATH'/tmp'
+
+# Experiment 05
+python -m ablation_study.AuT.VocalSound.aut_fix_analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+    --background_path $BASE_PATH'/data/DEMAND_16k' --output_path $BASE_PATH'/tmp' --batch_size 70 \
+    --max_epoch 20 --lr 1e-4 --nucnm_rate 1.0 --ent_rate 0.0 --gent_rate 0.0 --gent_q 2.0 --mse_rate 0.1 \
+    --orig_wght_pth './result/VocalSound/AMAuT/train' --seed 111089
+
+python -m ablation_study.AuT.VocalSound.stat_anal.analysis --dataset 'VocalSound' \
+    --dataset_root_path $BASE_PATH'/tmp/fix_clip_set' --output_path './result' \
+    --batch_size 32 --output_file_name 'fix-corruption_analysis05.csv' \
     --orig_wght_pth './result/VocalSound/AMAuT/train' \
     --adpt_wght_path $BASE_PATH'/tmp'
